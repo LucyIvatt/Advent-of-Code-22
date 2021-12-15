497b5d7734373ab374391b4022c58da1aff8ed9c
Lucy Ivatt <li571@york.ac.uk>
2021-12-15 00:12:35 +0000

    finished day 11 part one

---
+++ Day 11 Dumbo Octopus/Main.py
@@ -0,0 +1,81 @@
+def importList(filename):
+    formation = []
+    with open(filename, "r") as file:
+        for line in file:
+            formation.append([int(char) for char in line.strip()])
+    return formation
+
+
+def simulate_step(formation):
+    flashed = [[False for j in range(len(formation[i]))]
+               for i in range(len(formation))]
+
+    # Increases energy of each octopus by 1
+    formation = [[octopus + 1 for octopus in row] for row in formation]
+
+    # Energy level greater than 9 flashes
+    finished_flashing = False
+    while not finished_flashing:
+        for i in range(len(formation)):
+            for j in range(len(formation[i])):
+                if formation[i][j] > 9 and flashed[i][j] == False:
+                    flashed[i][j] = True
+                    for k, l in get_neighbors_coords(formation, i, j):
+                        formation[k][l] += 1
+
+        # Checks if all values > 9 have already flashed
+        finished_flashing = True
+        for i in range(len(formation)):
+            for j in range(len(formation[i])):
+                if formation[i][j] > 9 and flashed[i][j] == False:
+                    finished_flashing = False
+            if not finished_flashing:
+                break
+
+    # Calculates number of flashes
+    flash_count = sum(row.count(True) for row in flashed)
+
+    # Sets flashed octopuses to
+    formation = [[octopus if octopus <= 9 else 0 for octopus in row]
+                 for row in formation]
+
+    return(formation, flash_count)
+
+
+def get_neighbors_coords(formation, x, y):
+    neighbors_coords = []
+    for dx, dy in [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]:
+        new_x, new_y = x + dx, y + dy
+        if new_x >= 0 and new_x < len(formation):
+            if new_y >= 0 and new_y < len(formation[new_x]):
+                neighbors_coords.append((new_x, new_y))
+    return neighbors_coords
+
+
+def part_one():
+    formation = importList("Day 11 Dumbo Octopus\input.txt")
+    total_flashes = 0
+    for i in range(100):
+        formation, flashes = simulate_step(formation)
+        total_flashes += flashes
+    return total_flashes
+
+
+def print_formation(formation):
+    print("-----------------------")
+    for line in formation:
+        line_string = ""
+        for val in line:
+            if val > 9:
+                line_string += "F"
+            else:
+                line_string += str(val)
+        print(line_string)
+    print("-----------------------")
+
+
+print("--------------------------------------")
+print("DAY 11: DUMBO OCTOPUS")
+print("Part One Answer: " + str(part_one()))
+# print("Part Two Answer: " + str(part_two()))
+print("--------------------------------------")
