3ddc93f44606cc238076a4403a1a99d385f465c0
Lucy <li571@york.ac.uk>
2022-12-04 17:49:24 +0000

    finished day 2

--- day2/solution.py
+++ day2/solution.py
@@ -6,6 +6,14 @@ class Move(Enum):
     PAPER = 2
     SCISSORS = 3
 
+# Win
+# 1, 2, 3 --> 3, 1, 2
+# mod(x+1) (+1)
+
+# Loss
+# 1, 2, 3 --> 2, 3, 1
+# mod(x)+1 1, 2 , 0
+
 
 class Result(Enum):
     Win = 6
@@ -26,6 +34,15 @@ OUTCOME = {"X": Result.Loss,
            "Z": Result.Win}
 
 
+def match_outcome(move1, move2):
+    if move2.value == ((move1.value + 1) % 3) + 1:
+        return Result.Win
+    elif move2.value == ((move1.value) % 3) + 1:
+        return Result.Loss
+    else:
+        return Result.Draw
+
+
 def input_data(filename):
     """Returns the data imported from file - pairs of strategy guide information
     """
@@ -42,15 +59,7 @@ def part_1(input):
 
     # Win, Loss & Draw conditions
     for opp_move, your_move in input:
-        print(opp_move)
-        print(your_move)
-        if your_move.value == (opp_move.value - 1) % 3:
-            outcome = Result.Win
-        elif your_move.value == (opp_move.value + 1) % 3:
-            outcome = Result.Loss
-        else:
-            outcome = Result.Draw
-        print(outcome)
+        outcome = match_outcome(your_move, opp_move)
 
         # Score of one match is shape + round outcome
         score += your_move.value + outcome.value
@@ -64,19 +73,20 @@ def part_2(input):
     for opp_move, outcome in input:
         match outcome:
             case(Result.Win):
-                your_move = Move((opp_move.value + 1) % 3)
+                your_move = Move(((opp_move.value) % 3) + 1)
             case(Result.Draw):
                 your_move = opp_move
             case(Result.Loss):
-                your_move = Move((opp_move.value - 1) % 3)
+                your_move = Move(((opp_move.value + 1) % 3) + 1)
 
         # Score of one match is shape + round outcome
         score += your_move.value + outcome.value
+
     return score
 
 
 input_1 = input_data("day2/input.txt")
-input_2 = input_data("day2/example.txt")
+input_2 = input_data("day2/input.txt")
 
 print("--------------------------------------")
 print("Day 2: Rock Paper Scissors")
--- day2/test.py
+++ day2/test.py
@@ -1,6 +1,6 @@
 import unittest
 
-from day2.solution import input_data, part_1, part_2
+from day2.solution import input_data, match_outcome, part_1, part_2, Move, Result
 
 
 class TestDay2(unittest.TestCase):
@@ -8,6 +8,23 @@ class TestDay2(unittest.TestCase):
     def setUpClass(cls):
         cls.example = input_data("day2/example.txt")
         cls.input = input_data("day2/input.txt")
+
+    def test_match_outcome_win(self):
+        move1 = [Move.ROCK, Move.PAPER, Move.SCISSORS]
+        move2 = [Move.SCISSORS, Move.ROCK, Move.PAPER]
+        for i in range(3):
+            self.assertEqual(match_outcome(move1[i], move2[i]), Result.Win)
+
+    def test_match_outcome_loss(self):
+        move1 = [Move.ROCK, Move.PAPER, Move.SCISSORS]
+        move2 = [Move.PAPER, Move.SCISSORS, Move.ROCK]
+        for i in range(3):
+            self.assertEqual(match_outcome(move1[i], move2[i]), Result.Loss)
+
+    def test_match_outcome_draw(self):
+        move = [Move.ROCK, Move.PAPER, Move.SCISSORS]
+        for i in range(3):
+            self.assertEqual(match_outcome(move[i], move[i]), Result.Draw)
 
     def test_day_2_p1_example(self):
         """
@@ -36,7 +53,8 @@ class TestDay2(unittest.TestCase):
         Tests the Day 2 Part 2 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        pass
+        answer = part_2(self.__class__.input)
+        self.assertEqual(answer, 13_131)
 
 
 if __name__ == '__main__':
