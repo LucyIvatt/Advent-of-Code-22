47b42fa5d746aa6bd14a8173c3513d5e8dab7fa2
Lucy <li571@york.ac.uk>
2022-12-04 19:38:40 +0000

    completed day 4

---
+++ day4/solution.py
@@ -0,0 +1,49 @@
+def input_data(filename):
+    """Returns the data imported from file - lists containing pairs of elf assignments [(start, end), (start2, end2)]
+    """
+    file = open(filename, "r")
+    input = file.readlines()
+    file.close()
+    input = [[(int(x[0].split("-")[0]),
+               int(x[0].split("-")[1])),
+              (int(x[1].split("-")[0]),
+              int(x[1].split("-")[1]))]
+             for x in (line.strip().split(",") for line in input)]
+    return input
+
+
+def part_one(input):
+    """Counts number of assignment pairs where one fully overlaps with the other."""
+    count = 0
+    for elf1, elf2 in input:
+        # Orders the elves by starting area id
+        if elf2[0] < elf1[0]:
+            elf1, elf2 = elf2, elf1
+
+        # Checks for full overlap
+        if (elf1[0] == elf2[0]) or (elf2[1] <= elf1[1]):
+            count += 1
+    return count
+
+
+def part_two(input):
+    """Counts number of assignment pairs which overlap at any point."""
+    count = 0
+    for elf1, elf2 in input:
+        # Orders the elves by starting area id
+        if elf2[0] < elf1[0]:
+            elf1, elf2 = elf2, elf1
+
+        # Checks for partial overlap
+        if not (elf1[1] < elf2[0]):
+            count += 1
+    return count
+
+
+input = input_data("day4/input.txt")
+
+print("--------------------------------------")
+print("Day 4: Camp")
+print("Part One Answer: " + str(part_one(input)))
+print("Part Two Answer: " + str(part_two(input)))
+print("--------------------------------------")
---
+++ day4/test.py
@@ -0,0 +1,40 @@
+import unittest
+
+from day4.solution import input_data, part_one, part_two
+
+
+class TestDay4(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.example = input_data("day4/example.txt")
+        cls.input = input_data("day4/input.txt")
+
+    def test_day_4_p1_example(self):
+        """
+        Tests Day 4 Part 1 using the example given in the scenario
+        """
+        self.assertEqual(part_one(self.__class__.example), 2)
+
+    def test_day_4_p1_actual(self):
+        """
+        Tests the Day 4 Part 1 using my generated input. Used to check any edits
+        made to the program have not broken it.
+        """
+        self.assertEqual(part_one(self.__class__.input), 657)
+
+    def test_day_4_p2_example(self):
+        """
+        Tests Day 4 Part 2 using the example given in the scenario.
+        """
+        self.assertEqual(part_two(self.__class__.example), 4)
+
+    def test_day_4_p2_actual(self):
+        """
+        Tests the Day 4 Part 2 using my generated input. Used to check any edits
+        made to the program have not broken it.
+        """
+        self.assertEqual(part_Two(self.__class__.input), 938)
+
+
+if __name__ == '__main__':
+    unittest.main()
