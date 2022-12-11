2ba1176b831edc592a9813dc08b1e0280b481912
Lucy <li571@york.ac.uk>
2022-12-11 16:00:35 +0000

    cleaned up day 11 and added comments

--- day11/solution.py
+++ day11/solution.py
@@ -1,7 +1,6 @@
 import operator
 import re
 from copy import deepcopy
-import time
 from math import prod
 
 
@@ -23,17 +22,22 @@ class Monkey:
         self.inspect_num = 0
 
     def inspect(self, item, modulo=None):
+        """ Inspects an item and returns the monkey to send it to and the items new worry level."""
         self.inspect_num += 1
+
+        # Applies the operation to the item
         if self.op_num == "old":
             item = self.op(item, item)
         else:
             item = self.op(item, self.op_num)
 
+        # Reduces worry level
         if modulo is not None:
             item %= modulo
         else:
             item //= 3
 
+        # Sends item to the correct monkey
         if item % self.test_num == 0:
             return self.true_m, item
         else:
@@ -41,6 +45,8 @@ class Monkey:
 
 
 def input_data(filename):
+    """ Reads the input file and returns a dictionary of monkeys."""
+    # Reads file and splits on each monkey.
     file = open(filename, "r")
     input = file.read().split("\n\n")
     file.close()
@@ -50,41 +56,41 @@ def input_data(filename):
         monkey = monkey.split("\n ")
         args = []
 
+        # Parses the arguments by finding the numbers and operation.
         for i in range(len(monkey)):
-            if i == 1:
+            if i == 1:  # List of items
                 args.append(re.findall(r'\d+', monkey[i]))
-            elif i == 2:
+            elif i == 2:  # Operation
                 operation = re.search(r'[-+\/*] (\d+|old)', monkey[i]).group()
                 args.extend(operation.split(" "))
-            else:
+            else:  # All other arguments
                 args.append(re.search(r'\d+', monkey[i]).group())
 
+        # Creates a monkey object and adds it to the dictionary.
         monkeys[int(args[0])] = Monkey(*args[1:])
     return monkeys
 
 
-def part_one(monkeys):
+def solution(monkeys, part_two=False):
     monkeys = deepcopy(monkeys)
-    for _ in range(20):
-        for id, monkey in monkeys.items():
-            for item in monkey.items:
-                new_monkey, new_item = monkey.inspect(item)
-                monkeys[new_monkey].items.append(new_item)
-            monkey.items.clear()
-    inspect_nums = sorted([monkey.inspect_num for monkey in monkeys.values()])
-    monkey_business = inspect_nums[-1] * inspect_nums[-2]
-    return monkey_business
 
+    # Sets the number of rounds and the modulo if required.
+    if not part_two:
+        round_num = 20
+        modulo = None
+    else:
+        round_num = 10_000
+        modulo = prod(monkey.test_num for monkey in monkeys.values())
 
-def part_two(monkeys):
-    monkeys = deepcopy(monkeys)
-    modulo = prod(monkey.test_num for monkey in monkeys.values())
-    for i in range(10_000):
-        for id, monkey in monkeys.items():
+    # Plays out the number of rounds required
+    for _ in range(round_num):
+        for monkey in monkeys.values():
             for item in monkey.items:
                 new_monkey, new_item = monkey.inspect(item, modulo)
                 monkeys[new_monkey].items.append(new_item)
             monkey.items.clear()
+
+    # Finds the two monkeys with the highest inspect numbers and calculates monkey business.
     inspect_nums = sorted([monkey.inspect_num for monkey in monkeys.values()])
     monkey_business = inspect_nums[-1] * inspect_nums[-2]
     return monkey_business
@@ -94,6 +100,6 @@ input = input_data("day11/input.txt")
 
 print("--------------------------------------")
 print("Day 11: Monkey in the Middle")
-print("Part One Answer: " + str(part_one(input)))
-print("Part Two Answer: " + str(part_two(input)))
+print("Part One Answer: " + str(solution(input)))
+print("Part Two Answer: " + str(solution(input, part_two=True)))
 print("--------------------------------------")
--- day11/test.py
+++ day11/test.py
@@ -1,7 +1,6 @@
 import unittest
 
-# TODO: import other solution methods
-from day11.solution import input_data, part_one, part_two
+from day11.solution import input_data, solution
 
 
 class TestDay11(unittest.TestCase):
@@ -14,27 +13,27 @@ class TestDay11(unittest.TestCase):
         """
         Tests Day 11 Part 1 using the example given in the scenario
         """
-        self.assertEqual(part_one(self.example), 10_605)
+        self.assertEqual(solution(self.example), 10_605)
 
     def test_day_11_p1_actual(self):
         """
         Tests the Day 11 Part 1 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        self.assertEqual(part_one(self.input), 50_172)
+        self.assertEqual(solution(self.input), 50_172)
 
     def test_day_11_p2_example(self):
         """
         Tests Day 11 Part 2 using the example given in the scenario.
         """
-        self.assertEqual(part_two(self.example), 2_713_310_158)
+        self.assertEqual(solution(self.example, part_two=True), 2_713_310_158)
 
     def test_day_11_p2_actual(self):
         """
         Tests the Day 11 Part 2 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        self.assertEqual(part_two(self.input), 11_614_682_178)
+        self.assertEqual(solution(self.input, part_two=True), 11_614_682_178)
 
 
 if __name__ == '__main__':
