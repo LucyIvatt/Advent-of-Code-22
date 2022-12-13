903657831bcc071610b0b0058353f67f778be14a
Lucy <li571@york.ac.uk>
2022-12-13 19:47:51 +0000

    finished day 13

--- day13/solution.py
+++ day13/solution.py
@@ -1,76 +1,92 @@
-import numpy as np
+from functools import total_ordering
+from numpy import prod
 
 
-class Pair:
-    def __init__(self, p1, p2):
-        pass
+@total_ordering
+class Packet():
+    def __init__(self, string, divider=False):
+        self.packet = eval(string)
+        self.divider = divider
 
-    def build_tree(self, pi):
-        pass
+    def __str__(self):
+        return str(self.packet)
 
+    def __eq__(self, other):
+        return self.compare(self.packet, other.packet) == 0
 
-def compare(x, y):
-    """1 if x < y (Correct order), 0 if x == y, -1 if x > y (incorrect order)"""
-    # When both values are integers
-    if type(x) == int and type(y) == int:
-        if x < y:
-            return 1
-        elif x == y:
-            return 0
-        else:
-            return -1
+    def __lt__(self, other):
+        return self.compare(self.packet, other.packet) == 1
 
-    # When one value is a list and the other is an integer - convert integer to list
-    if type(x) == list and type(y) == int:
-        y = [y]
-    elif type(x) == int and type(y) == list:
-        x = [x]
-
-    # When both values are lists
-    if type(x) == list and type(y) == list:
-        if len(y) < len(x):
-            return -1
-        elif len(y) > len(x):
-            if np.all(np.asarray(y[:len(x)]) >= np.asarray(x)):
+    @staticmethod
+    def compare(x, y):
+        """1 if x < y (Correct order), 0 if x == y, -1 if x > y (incorrect order)"""
+        # When both values are integers
+        if type(x) == int and type(y) == int:
+            if x < y:
                 return 1
+            elif x == y:
+                return 0
             else:
                 return -1
-        else:
-            if np.all(np.asarray(y) >= np.asarray(x)):
-                if np.any(np.asarray(y) > np.asarray(x)):
+
+        # When one value is a list and the other is an integer - convert integer to list
+        if type(x) == list and type(y) == int:
+            y = [y]
+        elif type(x) == int and type(y) == list:
+            x = [x]
+
+        # When both values are lists - need to recursively compare each element
+        if type(x) == list and type(y) == list:
+            i = 0
+            while i < len(x) and i < len(y):
+                # If there are still comparable elements, recursively compare them
+                result = Packet.compare(x[i], y[i])
+
+                if result == 1:
                     return 1
-                else:
-                    return 0
-            else:
+                if result == -1:
+                    return -1
+                i += 1
+
+            if i == len(x) and i < len(y):
+                return 1
+            elif i == len(x) and i == len(y):
+                return 0
+            elif i < len(x) and i == len(y):
                 return -1
 
 
 def input_data(filename):
-    """Returns the data imported from file -
+    """Returns the data imported from file - strings of pairs (separated by newline)
     """
-    file = open(filename, "r")
-    input = file.readlines()
-    file.close()
-
-    # split input on blank lines and remove \n characters
-    input = [x.strip() for x in input if x.strip() != ""]
-    input = [(input[i], input[i+1]) for i in range(0, len(input), 2)]
-    return input
-# TODO: Enter solutions
+    with open(filename, 'r') as file:
+        return file.read().strip().split("\n\n")
 
 
 def part_one(input):
-    """Returns the solution to part one - sum of indicies 
+    """Returns the solution to part one - sum of indicies
     of pairs in correct order"""
-    for pair in input:
-        print(pair)
+    sum = 0
+    for i, block in enumerate(input):
+        p1, p2 = [Packet(x) for x in block.split("\n")]
+        if p1 < p2:
+            sum += i + 1
+    return sum
 
 
-input = input_data("day13/example.txt")
-part_one(input)
+def part_two(input):
+    packets = [Packet(x, divider=True) for x in ["[[2]]", "[[6]]"]]
+    for block in input:
+        packets.extend([Packet(x) for x in block.split("\n")])
+    packets = sorted(packets)
+    return prod([packets.index(packet)+1 for packet in packets if packet.divider == True])
+
+
+input = input_data("day13/input.txt")
+
 
 print("--------------------------------------")
 print("Day 13: Distress Signal")
-print("Part One Answer: ")
-print("Part Two Answer: ")
+print("Part One Answer: " + str(part_one(input)))
+print("Part Two Answer: " + str(part_two(input)))
 print("--------------------------------------")
--- day13/test.py
+++ day13/test.py
@@ -1,70 +1,72 @@
 import unittest
 
 # TODO: import other solution methods
-from day13.solution import input_data, compare
+from day13.solution import Packet, input_data, part_one, part_two
 
 
 class TestDay13(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
+
         cls.example = input_data("day13/example.txt")
         cls.input = input_data("day13/input.txt")
 
-    def test_compare(self):
+    def test_compare_basic(self):
         # Basic ints
-        self.assertEqual(compare(1, 2), 1)
-        self.assertEqual(compare(2, 2), 0)
-        self.assertEqual(compare(2, 1), -1)
+        self.assertEqual(Packet.compare(1, 2), 1)
+        self.assertEqual(Packet.compare(2, 2), 0)
+        self.assertEqual(Packet.compare(2, 1), -1)
 
         # Basic lists - right list bigger
-        self.assertEqual(compare([1, 2], [2, 3, 4]), 1)
-        self.assertEqual(compare([3, 2], [2, 3, 4]), -1)
+        self.assertEqual(Packet.compare([1, 2], [2, 3, 4]), 1)
+        self.assertEqual(Packet.compare([1, 5], [2, 4, 2]), 1)
+        self.assertEqual(Packet.compare([3, 2], [2, 3, 4]), -1)
 
         # Basic lists - left list bigger
-        self.assertEqual(compare([1, 2, 3], [2, 3]), -1)
-        self.assertEqual(compare([1, 2, 3], [0, 1]), -1)
-        self.assertEqual(compare([1, 2, 3], [1, 2]), -1)
+        self.assertEqual(Packet.compare([1, 2, 3], [2, 3]), 1)
+        self.assertEqual(Packet.compare([1, 2, 3], [0, 1]), -1)
+        self.assertEqual(Packet.compare([1, 2, 3], [1, 2]), -1)
 
         # Basic lists - list same length
-        self.assertEqual(compare([1, 2], [1, 3]), 1)
-        self.assertEqual(compare([1, 2], [1, 2]), 0)
-        self.assertEqual(compare([2, 2], [1, 2]), -1)
+        self.assertEqual(Packet.compare([1, 2], [1, 3]), 1)
+        self.assertEqual(Packet.compare([1, 2], [1, 2]), 0)
+        self.assertEqual(Packet.compare([2, 2], [1, 2]), -1)
 
         # Int left, list right
-        self.assertEqual(compare(1, [1, 2]), 1)
-        self.assertEqual(compare(3, [1]), -1)
-        self.assertEqual(compare(2, [2]), 0)
+        self.assertEqual(Packet.compare(1, [1, 2]), 1)
+        self.assertEqual(Packet.compare(3, [1]), -1)
+        self.assertEqual(Packet.compare(2, [2]), 0)
 
         # List left, int right
-        self.assertEqual(compare([1], 2), 1)
-        self.assertEqual(compare([3], 2), -1)
-        self.assertEqual(compare([2], 2), 0)
+        self.assertEqual(Packet.compare([1], 2), 1)
+        self.assertEqual(Packet.compare([3], 2), -1)
+        self.assertEqual(Packet.compare([2], 2), 0)
 
     def test_day_13_p1_example(self):
         """
         Tests Day 13 Part 1 using the example given in the scenario
         """
-        pass
+        self.assertEqual(part_one(self.example), 13)
 
     def test_day_13_p1_actual(self):
         """
         Tests the Day 13 Part 1 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        pass
+        self.assertEqual(part_one(self.input), 5717)
 
     def test_day_13_p2_example(self):
         """
         Tests Day 13 Part 2 using the example given in the scenario.
         """
-        pass
+        self.assertEqual(part_two(self.example), 140)
 
     def test_day_13_p2_actual(self):
         """
         Tests the Day 13 Part 2 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        pass
+        self.assertEqual(part_two(self.input), 25_935)
 
 
 if __name__ == '__main__':
