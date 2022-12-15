189c4ec312cd8d400b29894f2965e53b276f4ca7
Lucy <li571@york.ac.uk>
2022-12-15 16:44:15 +0000

    working on day 15 p2

--- day15/solution.py
+++ day15/solution.py
@@ -36,12 +36,21 @@ def clear_area(sensor, distance, Y_indices, max_xy=None, p1=True):
     while x-distance+i <= x:
         for cy in y_coords:
             if p1 and cy == P1_Y:
-                for cx in range(x-distance+i, x+distance-i):
-                    Y_indices[P1_Y].add(cx)
+                Y_indices[P1_Y].append((x-distance+i, x+distance-i))
+
             elif not p1:
-                for cx in range(x-distance+i, x+distance-i+1):
-                    if 0 <= cx <= 20 and 0 <= cy <= 20:
-                        Y_indices[cy].add(cx)
+                if x-distance+i < 0:
+                    sx = 0
+                else:
+                    sx = x-distance+i
+
+                if x+distance-i > max_xy:
+                    bx = max_xy
+                else:
+                    bx = x+distance-i
+
+                if 0 <= cy <= max_xy:
+                    Y_indices[cy].append((sx, bx))
         if len(y_coords) == 1:
             y_coords = [y+1, y-1]
         else:
@@ -51,28 +60,61 @@ def clear_area(sensor, distance, Y_indices, max_xy=None, p1=True):
 
 
 def part_one(input):
-    Y_indices = {P1_Y: set()}
+    Y_indices = {P1_Y: list()}
     for sensor, distance in input.items():
         Y_indices = clear_area(sensor, distance, Y_indices)
-    return len(Y_indices[P1_Y])
+
+    intervals = [[x, y] for x, y in Y_indices[P1_Y]]
+    intervals = merge(intervals)
+    sum = 0
+    for x1, x2 in intervals:
+        sum += x2-x1
+    return sum
+
+
+def is_overlaping(a, b):
+    if b[0] >= a[0] and b[0] <= a[1]:
+        return True
+    elif a[1]+1 == b[0]:
+        return True
+    else:
+        return False
+
+# merge the intervals
+
+
+def merge(arr):
+    # sort the intervals by its first value
+    arr.sort(key=lambda x: x[0])
+
+    merged_list = []
+    merged_list.append(arr[0])
+    for i in range(1, len(arr)):
+        pop_element = merged_list.pop()
+        if is_overlaping(pop_element, arr[i]):
+            new_element = pop_element[0], max(pop_element[1], arr[i][1])
+            merged_list.append(new_element)
+        else:
+            merged_list.append(pop_element)
+            merged_list.append(arr[i])
+    return merged_list
 
 
 def part_two(input):
-    max_xy = 20
-    Y_indices = {y: set() for y in range(max_xy+1)}
+    max_xy = 4_000_000
+    Y_indices = {y: [] for y in range(max_xy+1)}
     for sensor, distance in input.items():
-        print("Clearing area of sensor: " + str(sensor) +
-              " with distance: " + str(distance))
+        print("Clearing sensor: " + str(sensor))
         Y_indices = clear_area(sensor, distance, Y_indices, max_xy, p1=False)
-    bx, by = find_beacon(Y_indices, max_xy)
+    for k, v in Y_indices.items():
+        intervals = [[x, y] for x, y in Y_indices[k]]
+        Y_indices[k] = merge(intervals)
+        if len(Y_indices[k]) > 1:
+            bx = Y_indices[k][0][1]
+            by = k
+            print(bx, by)
+            break
     return bx * 4_000_000 + by
-
-
-def find_beacon(Y_blocked_pos, max_xy):
-    for y in range(max_xy):
-        for x in range(max_xy):
-            if x not in Y_blocked_pos[y]:
-                return (x, y)
 
 
 P1_Y = 10
