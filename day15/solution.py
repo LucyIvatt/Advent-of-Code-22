f4c8faa7029aa6bff0e717680f96423c9c262339
Lucy <li571@york.ac.uk>
2022-12-15 18:07:49 +0000

    neatened and added comments

--- day15/solution.py
+++ day15/solution.py
@@ -1,8 +1,8 @@
 import re
-from collections import defaultdict
 
 
 def manhattan_distance(point1, point2):
+    """Returns manhattan distance between two points."""
     return sum(abs(value1 - value2) for value1, value2 in zip(point1, point2))
 
 
@@ -15,101 +15,105 @@ def input_data(filename):
 
     coords = [[int(x) for x in re.findall(r'-?\d+\.?\d*', line)]
               for line in input]
-    sens_dists = {(int(c[0]), int(c[1])): manhattan_distance(
+    return {(int(c[0]), int(c[1])): manhattan_distance(
         (c[0], c[1],), (c[2], c[3])) for c in coords}
 
-    return sens_dists
 
-
-def clear_area(sensor, distance, Y_indices, part_check, p1=True):
-    """Clears the area around the sensor"""
+def clear_area(sensor, distance, Y_indices, bound, p1=True):
+    """Clears the area around the sensor."""
     x, y = sensor
     i = 0
     y_coords = [y]
 
-    if p1 and part_check not in range(y-distance, y+distance+1):
+    # if the sensor will not affect the y coordinate thats being checked in part 1 - skip
+    if p1 and bound not in range(y-distance, y+distance+1):
         return Y_indices
 
-    if not p1 and (y-distance <= 0) and (y+distance+1 >= part_check):
+    # if the sensor will not affect the y coordinates that are being checked in part 2 - skip
+    if not p1 and (y-distance <= 0) and (y+distance+1 >= bound):
         return Y_indices
 
-    while x-distance+i <= x:
+    for i in range(distance):
+        start, end = x-distance+i, x+distance-i
         for cy in y_coords:
-            if p1 and cy == part_check:
-                Y_indices[part_check].append((x-distance+i, x+distance-i))
-
+            # Only updates the y coordinate being checked in part 1
+            if p1 and cy == bound:
+                Y_indices[bound].append((start, end))
             elif not p1:
-                if x-distance+i < 0:
-                    sx = 0
-                else:
-                    sx = x-distance+i
+                # Clamps the start and end values to between 0 and 4,000,000 if out of range
+                if start < 0:
+                    start = 0
+                elif end > bound:
+                    end = bound
 
-                if x+distance-i > part_check:
-                    bx = part_check
-                else:
-                    bx = x+distance-i
+                # Updates the interval dictionary if the y coordinate is within the range
+                if 0 <= cy <= bound:
+                    Y_indices[cy].append((start, end))
 
-                if 0 <= cy <= part_check:
-                    Y_indices[cy].append((sx, bx))
+        # Updates the y coordinate after each loop
         if len(y_coords) == 1:
             y_coords = [y+1, y-1]
         else:
             y_coords = [y_coords[0]+1, y_coords[1]-1]
-        i += 1
+
     return Y_indices
 
 
 def is_overlaping(a, b):
+    "Determines if two ordered intervals are overlapping or adjacent."
+    # Overlapping
     if b[0] >= a[0] and b[0] <= a[1]:
         return True
+    # Adjacent
     elif a[1]+1 == b[0]:
         return True
     else:
         return False
 
-# merge the intervals
 
+def merge_intervals(intervals):
+    """Takes in the intervals and merges them if they are overlapping or adjacent."""
+    # Sorts the intervals by the start x coordinate
+    intervals = [[x, y] for x, y in intervals]
+    intervals.sort(key=lambda x: x[0])
+    merged = [intervals[0]]
 
-def merge(arr):
-    # sort the intervals by its first value
-    arr.sort(key=lambda x: x[0])
-    merged_list = []
-    merged_list.append(arr[0])
-    for i in range(1, len(arr)):
-        pop_element = merged_list.pop()
-        if is_overlaping(pop_element, arr[i]):
-            new_element = pop_element[0], max(pop_element[1], arr[i][1])
-            merged_list.append(new_element)
+    for i in range(1, len(intervals)):
+        # Merge intervals if they are overlapping or adjacent
+        pop_element = merged.pop()
+        if is_overlaping(pop_element, intervals[i]):
+            new_element = pop_element[0], max(pop_element[1], intervals[i][1])
+            merged.append(new_element)
         else:
-            merged_list.append(pop_element)
-            merged_list.append(arr[i])
-    return merged_list
+            merged.append(pop_element)
+            merged.append(intervals[i])
+    return merged
 
 
 def part_one(input, p1_y):
+    """Returns the number of impossible position when y=p1_y."""
     Y_indices = {p1_y: list()}
     for sensor, distance in input.items():
         Y_indices = clear_area(sensor, distance, Y_indices, p1_y)
-
-    intervals = [[x, y] for x, y in Y_indices[p1_y]]
-    intervals = merge(intervals)
-    sum = 0
-    for x1, x2 in intervals:
-        sum += x2-x1
-    return sum
+    intervals = merge_intervals(Y_indices[p1_y])
+    return sum(x2-x1 for x1, x2 in intervals)
 
 
 def part_two(input, max_xy):
+    """Returns the tuning frequency of the distress beacon by finding its location"""
     Y_indices = {y: [] for y in range(max_xy+1)}
     for sensor, distance in input.items():
         Y_indices = clear_area(sensor, distance, Y_indices, max_xy, p1=False)
+
+    # Merges the intervals for each y coordinate, stops if it finds the one with a gap
     for k, v in Y_indices.items():
-        intervals = [[x, y] for x, y in Y_indices[k]]
-        Y_indices[k] = merge(intervals)
+        Y_indices[k] = merge_intervals(v)
         if len(Y_indices[k]) > 1:
             bx = Y_indices[k][0][1]+1
             by = k
             break
+
+    # Returns the tuning frequency
     return bx * 4_000_000 + by
 
 
