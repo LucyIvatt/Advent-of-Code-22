445b8bfb34fb6b05e5b08c6361f956e305158c85
Lucy <li571@york.ac.uk>
2022-12-15 19:00:38 +0000

    updated folder names to include task names, updating new_day.sh script to use this naming scheme

--- new_day.sh
+++ new_day.sh
@@ -5,7 +5,7 @@ day_num="$1"
 task_name="$2"
 year="$3"
 
-day_folder="$year/day$day_num"
+day_folder="$year/$day_num $task_name"
 
 cp -rv template "$day_folder"
 jinja2 "$day_folder/solution.py.j2" -o "$day_folder/solution.py" -D "day_num=$day_num" -D "task_name=$task_name"
