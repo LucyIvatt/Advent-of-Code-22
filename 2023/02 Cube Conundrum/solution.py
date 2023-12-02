b6722210e0cac9481d824c1cb5a484360755851f
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-02 17:39:41 +0000

    minor refactor of solutions for day 2 2023

--- 2023/02 Cube Conundrum/solution.py
+++ 2023/02 Cube Conundrum/solution.py
@@ -1,4 +1,6 @@
 from functools import reduce
+from collections import defaultdict 
+import operator
 
 CONSTRAINTS = {"red": 12, "green": 13, "blue": 14}
 
@@ -13,50 +15,44 @@ def input_data(filename):
 
 def parse_games(puzzle_input):
     game_dict = {}
+
     for line in puzzle_input:
         rounds_list = []
         game_id, rounds = line.strip().split(":")
+
         for turn in rounds.split(";"):
-            cubes_dict = {}
-            for colour_set in turn.split(", "):
-                number, colour = colour_set.strip().split(" ")
-                cubes_dict[colour] = int(number)
+            cubes_dict = {colour: int(number) for number, colour in (cubes.strip().split(" ") for cubes in turn.split(", "))}
             rounds_list.append(cubes_dict)
+            
         game_dict[game_id.split(" ")[1]] = rounds_list
+    
     return game_dict
+
 
 def part_one(puzzle_input):
     game_dict = parse_games(puzzle_input)
     id_sum = 0
 
     for game, rounds in game_dict.items():
-        game_valid = True
-        for turn in rounds:
-            for colour, number in turn.items():
-                if number > CONSTRAINTS[colour]:
-                    game_valid = False
-                    break
-        
-        if game_valid: id_sum += int(game)
-    
+        if all(number <= CONSTRAINTS[colour] for turn in rounds for colour, number in turn.items()):
+            id_sum += int(game)
+
     return id_sum
 
 def part_two(puzzle_input):
     game_dict = parse_games(puzzle_input)
     power_sum = 0
 
-    for game, rounds in game_dict.items():
-        min_cubes = {colour: 0 for colour in CONSTRAINTS.keys()}
+    for rounds in game_dict.values():
+        min_cubes = defaultdict(int) 
         for turn in rounds:
             for colour, number in turn.items():
-                if number > min_cubes[colour]: min_cubes[colour] = number
-        power_sum += reduce((lambda x, y: x * y), min_cubes.values())
-    
+                min_cubes[colour] = max(min_cubes[colour], number)
 
-
-    
+        power_sum += reduce(operator.mul, min_cubes.values())
     
     return power_sum
+
 
 puzzle_input = input_data("2023/02 Cube Conundrum/input.txt")
 
