67ca7248a2dafb2e34b181d37c400dd704042e3a
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-03 16:39:57 +0000

    added no diagonal functionality to aoc helper for adjacent coordinates and added relevant unit tests

--- helpers/aoc_utils.py
+++ helpers/aoc_utils.py
@@ -5,8 +5,13 @@ def input_data(filename):
         puzzle_input = [line.strip() for line in file.readlines()]
         return puzzle_input
 
-def get_adjacent_and_diagonal_coords(coords, x_limit, y_limit):
-    directions = [(dx, dy) for dx in range(-1, 2) for dy in range(-1, 2) if (dx, dy) != (0, 0)]
+def get_adjacent_coords(coords, x_limit, y_limit, diagonal=False):
+    '''Returns all of the adjacent coordinates, including diagonals) for a list of coordinates. Removes any duplicates and any coordinates
+    that were present in the original input.'''
+    if diagonal:
+        directions = [(dx, dy) for dx in range(-1, 2) for dy in range(-1, 2) if (dx, dy) != (0, 0)]
+    else:
+        directions = [(dx, dy) for dx in range(-1, 2) for dy in range(-1, 2) if (dx, dy) != (0, 0) and (dx == 0 or dy == 0)]
 
     adjecent_coords = []
 
--- year_2023/day_03_gear_ratios/solution.py
+++ year_2023/day_03_gear_ratios/solution.py
@@ -1,6 +1,6 @@
 import re
 from collections import defaultdict 
-from helpers.aoc_utils import input_data, get_adjacent_and_diagonal_coords
+from helpers.aoc_utils import input_data, get_adjacent_coords
 
 DIGIT_REGEX = r'\b\d+\b'
 
@@ -10,7 +10,7 @@ def process_matches(puzzle_input, p2=False):
     for i, line in enumerate(puzzle_input):
         for match in re.finditer(DIGIT_REGEX, line):
             number_positions = [(i, y) for y in range(match.start(), match.end())]
-            adj_coords = get_adjacent_and_diagonal_coords(number_positions, len(line), len(puzzle_input))
+            adj_coords = get_adjacent_coords(number_positions, len(line), len(puzzle_input))
 
             if not p2 and any(puzzle_input[line][char] != '.' for line, char in adj_coords):
                 result += int(match.group())
