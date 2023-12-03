275e52e0dc7ab929bc3cbca1e475c15c434ff60a
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-03 16:46:54 +0000

    added comments in day 3

--- year_2023/day_03_gear_ratios/solution.py
+++ year_2023/day_03_gear_ratios/solution.py
@@ -13,18 +13,21 @@ def process_matches(puzzle_input, p2=False):
             adj_coords = get_adjacent_coords(coords=number_positions, x_limit=len(line), y_limit=len(puzzle_input), diagonal=True)
 
             if not p2 and any(puzzle_input[line][char] != '.' for line, char in adj_coords):
-                result += int(match.group())
+                result += int(match.group()) # sums part numbers that are adjacent to a symbol which is not '.'
             
-            if p2:
+            if p2: # adds part numbers to dictionary with their adjacent gear as the key
                 for adj_line, char in adj_coords:
                     if puzzle_input[adj_line][char] != '.':
                         result[(adj_line, char)].append(int(match.group()))
     return result
 
 def part_one(puzzle_input):
+    '''Adds together all part numbers that have a gear symbol adjacent to them.'''
     return process_matches(puzzle_input)
 
 def part_two(puzzle_input):
+    '''Adds gear symbols to a dictionary based on their location, with values being the part numbers that are adjacent.
+        Returns sum of the part numbers for gears with more than 1 part.'''
     return sum(numbers[0] * numbers[1] for numbers in process_matches(puzzle_input, p2=True).values() if len(numbers) > 1)
 
 puzzle_input = input_data("year_2023/day_03_gear_ratios/input.txt")
