b0b0504725de50cbb60164e38f099760bc74530e
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-03 12:26:46 +0000

    finished day 3 p1

--- helpers/aoc_utils_test.py
+++ helpers/aoc_utils_test.py
@@ -5,15 +5,21 @@ from helpers.aoc_utils import get_adjacent_and_diagonal_coords
 
 class TestDay03(unittest.TestCase):
 
-    def test_adj_and_diag_corners(self):
+    def test_adj_and_diag_corners_single(self):
         self.assertCountEqual(get_adjacent_and_diagonal_coords([(0, 0)], 3, 3), [(0, 1), (1, 0), (1, 1)])
         self.assertCountEqual(get_adjacent_and_diagonal_coords([(2, 0)], 3, 3), [(1, 0), (2, 1), (1, 1)])
 
         self.assertCountEqual(get_adjacent_and_diagonal_coords([(0, 2)], 3, 3), [(1, 1), (1, 2), (0, 1)])
         self.assertCountEqual(get_adjacent_and_diagonal_coords([(2, 2)], 3, 3), [(2, 1), (1, 1), (1, 2)])
     
-    def test_adj_diag_middle(self):
-        self.assertCountEqual(get_adjacent_and_diagonal_coords([(1, 1)], 3, 3), [(0, 0), (0,1), (0,2), (1, 0), (1, 2), (2, 0), (2, 1), (2,2)])
+    def test_adj_diag_middle_single(self):
+        self.assertCountEqual(get_adjacent_and_diagonal_coords([(1, 1)], 3, 3), 
+                              [(0, 0), (0,1), (0,2), (1, 0), (1, 2), (2, 0), (2, 1), (2,2)])
+    
+    def test_adj_diag_middle_multiple(self):
+        self.assertCountEqual(get_adjacent_and_diagonal_coords([(1, 1), (1,2)], 3, 4), 
+                            [(0, 0), (1,0), (2,0), (0, 1), (0, 2), (0, 3), (1, 3), (2,3), (2, 1), (2, 2)])
+
 
 
 
--- year_2023/day_03_gear_ratios/solution.py
+++ year_2023/day_03_gear_ratios/solution.py
@@ -1,25 +1,28 @@
 import re
+import string
 from helpers.aoc_utils import input_data, get_adjacent_and_diagonal_coords
 
 DIGIT_REGEX = pattern = r'\b\d+\b'
 
 def part_one(puzzle_input):
+    part_num_sum = 0
     for i in range(len(puzzle_input)):
         matches = re.finditer(pattern, puzzle_input[i])
 
         for match in matches:
             number_coords = [(i, y) for y in range(match.start(), match.end())]
-            print(number_coords)
+            adj_coords = get_adjacent_and_diagonal_coords(number_coords, len(puzzle_input[i]), len(puzzle_input))
+            has_symbols = any(puzzle_input[line][char] != '.' for line, char in adj_coords)
+            
+            if has_symbols:
+                part_num_sum += int(match.group())
+    return part_num_sum
 
+puzzle_input = input_data("year_2023/day_03_gear_ratios/input.txt")
 
-
-
-
-puzzle_input = input_data("year_2023/day_03_gear_ratios/example.txt")
-part_one(puzzle_input)
 
 print("--------------------------------------")
 print("Day 03: gear_ratios")
-print("Part One Answer: ")
+print(f"Part One Answer: {part_one(puzzle_input)}")
 print("Part Two Answer: ")
 print("--------------------------------------")
