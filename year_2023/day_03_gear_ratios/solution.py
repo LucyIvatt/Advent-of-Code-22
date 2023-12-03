e3c80f79d0cf25c3311cdc0837a943401b8c7000
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-03 15:55:53 +0000

    refactor part one day 3

--- year_2023/day_03_gear_ratios/solution.py
+++ year_2023/day_03_gear_ratios/solution.py
@@ -1,28 +1,25 @@
 import re
-import string
 from collections import defaultdict 
 from helpers.aoc_utils import input_data, get_adjacent_and_diagonal_coords
 
-DIGIT_REGEX = pattern = r'\b\d+\b'
+DIGIT_REGEX = r'\b\d+\b'
 
 def part_one(puzzle_input):
     part_num_sum = 0
-    for i in range(len(puzzle_input)):
-        matches = re.finditer(pattern, puzzle_input[i])
+    for i, line in enumerate(puzzle_input):
+        for match in re.finditer(DIGIT_REGEX, line):
+            number_positions = [(i, y) for y in range(match.start(), match.end())]
+            adj_coords = get_adjacent_and_diagonal_coords(number_positions, len(line), len(puzzle_input))
 
-        for match in matches:
-            number_coords = [(i, y) for y in range(match.start(), match.end())]
-            adj_coords = get_adjacent_and_diagonal_coords(number_coords, len(puzzle_input[i]), len(puzzle_input))
-            has_symbols = any(puzzle_input[line][char] != '.' for line, char in adj_coords)
-            
-            if has_symbols:
+            if any(puzzle_input[line][char] != '.' for line, char in adj_coords):
                 part_num_sum += int(match.group())
+
     return part_num_sum
 
 def part_two(puzzle_input):
     gear_dict = defaultdict(list)
     for i in range(len(puzzle_input)):
-        matches = re.finditer(pattern, puzzle_input[i])
+        matches = re.finditer(DIGIT_REGEX, puzzle_input[i])
 
         for match in matches:
             number_coords = [(i, y) for y in range(match.start(), match.end())]
