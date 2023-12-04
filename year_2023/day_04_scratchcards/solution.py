fc4af1cd41d87083cd0b36db05d019c7ef0ceea1
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-04 13:19:16 +0000

    finished day 4

--- year_2023/day_04_scratchcards/solution.py
+++ year_2023/day_04_scratchcards/solution.py
@@ -1,12 +1,54 @@
+from collections import Counter, defaultdict
+
 from helpers.aoc_utils import input_data, time_function
 
 puzzle_input = input_data("year_2023/day_04_scratchcards/input.txt")
 
 def part_one(puzzle_input):
-    pass
+    points = 0
+    for line in puzzle_input:
+        card = line.strip().split("|")
+        winning_numbers = [int(num) for num in card[0].split(":")[1].split()]
+        losing_numbers = [int(num) for num in card[1].split()]
+
+        matches = Counter(winning_numbers) & Counter(losing_numbers)
+
+        if len(matches)-1 >= 0:
+            points += pow(2, len(matches)-1)
+
+    return points
+    
 
 def part_two(puzzle_input):
-    pass
+    card_copies = defaultdict(int)
+
+    for i, line in enumerate(puzzle_input):
+        card = line.strip().split("|")
+
+        card_number = card[0].split(":")[0]
+        card_copies[card_number] += 1
+        winning_numbers = [int(num) for num in card[0].split(":")[1].split()]
+        losing_numbers = [int(num) for num in card[1].split()]
+
+        matches = Counter(winning_numbers) & Counter(losing_numbers)
+
+        for j in range(i+1, min(i+len(matches)+1, len(puzzle_input))):
+            new_card = puzzle_input[j].strip().split("|")
+            new_card_number = new_card[0].split(":")[0]
+            card_copies[new_card_number] += card_copies[card_number]
+    
+    return sum(card_copies.values())
+
+
+
+
+
+
+
+
+
+
+
 
 p1, p1_time = time_function(part_one, puzzle_input)
 p2, p2_time = time_function(part_two, puzzle_input)
--- year_2023/day_04_scratchcards/test.py
+++ year_2023/day_04_scratchcards/test.py
@@ -20,14 +20,13 @@ class TestDay04(unittest.TestCase):
         Tests the Day 04 Part 1 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        # self.assertEqual(part_one(self.__class__.puzzle_input), 0)
-        pass
+        self.assertEqual(part_one(self.__class__.puzzle_input), 17_782)
 
     def test_p2_example(self):
         """
         Tests Day 04 Part 2 using the example given in the scenario.
         """
-        # self.assertEqual(part_two(self.__class__.example), 0)
+        self.assertEqual(part_two(self.__class__.example), 30)
         pass
 
     def test_p2_actual(self):
@@ -35,7 +34,7 @@ class TestDay04(unittest.TestCase):
         Tests the Day 04 Part 2 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        # self.assertEqual(part_two(self.__class__.puzzle_input), 0)
+        self.assertEqual(part_two(self.__class__.puzzle_input), 8477787)
         pass
 
 if __name__ == '__main__':
