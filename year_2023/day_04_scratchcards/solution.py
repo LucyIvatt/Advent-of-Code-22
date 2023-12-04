ed5a46e0d4af99c74b56cfab7c7cb317d15329f1
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-04 09:50:24 +0000

    added day 4 boilerplate, updated test template slightly

---
+++ year_2023/day_04_scratchcards/solution.py
@@ -0,0 +1,18 @@
+from helpers.aoc_utils import input_data, time_function
+
+puzzle_input = input_data("year_2023/day_04_scratchcards/input.txt")
+
+def part_one(puzzle_input):
+    pass
+
+def part_two(puzzle_input):
+    pass
+
+p1, p1_time = time_function(part_one, puzzle_input)
+p2, p2_time = time_function(part_two, puzzle_input)
+
+print("--------------------------------------")
+print("Day 04: scratchcards")
+print(f"Part One Answer: {p1} - [{p1_time:.4f} seconds]")
+print(f"Part Two Answer: {p2} - [{p2_time:.4f} seconds]")
+print("--------------------------------------")
---
+++ year_2023/day_04_scratchcards/test.py
@@ -0,0 +1,42 @@
+import unittest
+
+from year_2023.day_04_scratchcards.solution import input_data, part_one, part_two
+
+class TestDay04(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.example = input_data("year_2023/day_04_scratchcards/example.txt")
+        cls.puzzle_input = input_data("year_2023/day_04_scratchcards/input.txt")
+
+    def test_p1_example(self):
+        """
+        Tests Day 04 Part 1 using the example given in the scenario
+        """
+        self.assertEqual(part_one(self.__class__.example), 13)
+        
+
+    def test_p1_actual(self):
+        """
+        Tests the Day 04 Part 1 using my generated input. Used to check any edits
+        made to the program have not broken it.
+        """
+        # self.assertEqual(part_one(self.__class__.puzzle_input), 0)
+        pass
+
+    def test_p2_example(self):
+        """
+        Tests Day 04 Part 2 using the example given in the scenario.
+        """
+        # self.assertEqual(part_two(self.__class__.example), 0)
+        pass
+
+    def test_p2_actual(self):
+        """
+        Tests the Day 04 Part 2 using my generated input. Used to check any edits
+        made to the program have not broken it.
+        """
+        # self.assertEqual(part_two(self.__class__.puzzle_input), 0)
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
