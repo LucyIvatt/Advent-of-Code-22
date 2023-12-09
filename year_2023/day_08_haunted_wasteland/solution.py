4061d99e833bc27fc97a2cc9d283c928aa3e2e5f
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-09 17:21:06 +0000

    finished day 8 p1

--- year_2023/day_08_haunted_wasteland/solution.py
+++ year_2023/day_08_haunted_wasteland/solution.py
@@ -1,8 +1,30 @@
 from helpers.aoc_utils import input_data, time_function
+import re
+
+PATTERN = re.compile(r"(\w+) = \((\w+), (\w+)\)")
 
 
 def part_one(puzzle_input):
-    pass
+    sequence = puzzle_input[0]
+    nodes = {match.group(1): (match.group(2), match.group(
+        3)) for input_string in puzzle_input[2:] if (match := PATTERN.match(input_string))}
+
+    not_found = True
+    steps = 0
+    current_node = "AAA"
+
+    while not_found:
+        for direction in sequence:
+
+            if direction == "L":
+                current_node = nodes[current_node][0]
+            else:
+                current_node = nodes[current_node][1]
+
+            steps += 1
+
+            if current_node == "ZZZ":
+                return steps
 
 
 def part_two(puzzle_input):
--- year_2023/day_08_haunted_wasteland/test.py
+++ year_2023/day_08_haunted_wasteland/test.py
@@ -2,26 +2,35 @@ import unittest
 
 from year_2023.day_08_haunted_wasteland.solution import input_data, part_one, part_two
 
+
 class TestDay08(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.example = input_data("year_2023/day_08_haunted_wasteland/example.txt")
-        cls.puzzle_input = input_data("year_2023/day_08_haunted_wasteland/input.txt")
+        cls.example = input_data(
+            "year_2023/day_08_haunted_wasteland/example.txt")
+        cls.example2 = input_data(
+            "year_2023/day_08_haunted_wasteland/example2.txt")
+        cls.puzzle_input = input_data(
+            "year_2023/day_08_haunted_wasteland/input.txt")
 
     def test_p1_example(self):
         """
         Tests Day 08 Part 1 using the example given in the scenario
         """
-        # self.assertEqual(part_one(self.__class__.example), 0)
-        pass
+        self.assertEqual(part_one(self.__class__.example), 2)
+
+    def test_p1_example_2(self):
+        """
+        Tests Day 08 Part 1 using example 2 given in the scenario
+        """
+        self.assertEqual(part_one(self.__class__.example2), 6)
 
     def test_p1_actual(self):
         """
         Tests the Day 08 Part 1 using my generated input. Used to check any edits
         made to the program have not broken it.
         """
-        # self.assertEqual(part_one(self.__class__.puzzle_input), 0)
-        pass
+        self.assertEqual(part_one(self.__class__.puzzle_input), 22357)
 
     def test_p2_example(self):
         """
@@ -37,6 +46,7 @@ class TestDay08(unittest.TestCase):
         """
         # self.assertEqual(part_two(self.__class__.puzzle_input), 0)
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
