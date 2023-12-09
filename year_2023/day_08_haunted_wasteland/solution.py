574e9058e5e801ff6ae846ae76bfc6a5746600e9
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-09 18:22:56 +0000

    finished day 8 refactor

--- year_2023/day_08_haunted_wasteland/solution.py
+++ year_2023/day_08_haunted_wasteland/solution.py
@@ -1,48 +1,44 @@
-from functools import reduce
 from helpers.aoc_utils import input_data, time_function
 import re
 import math
-import time
 
 PATTERN = re.compile(r"(\w+) = \((\w+), (\w+)\)")
 
 
 def find_num_steps(start, nodes, sequence, multiple_nodes=False):
-    steps = 0
-    current_node = start
+    steps, current_node = 0, start
 
     while True:
         for direction in sequence:
-
-            if direction == "L":
-                current_node = nodes[current_node][0]
-            else:
-                current_node = nodes[current_node][1]
+            left_node, right_node = nodes[current_node]
+            current_node = left_node if direction == "L" else right_node
 
             steps += 1
+
             if not multiple_nodes and current_node == "ZZZ":
                 return steps
             elif multiple_nodes and current_node.endswith("Z"):
                 return steps
 
 
-def part_one(puzzle_input):
+def parse_input(puzzle_input):
     sequence = puzzle_input[0]
     nodes = {match.group(1): (match.group(2), match.group(
         3)) for input_string in puzzle_input[2:] if (match := PATTERN.match(input_string))}
+    return sequence, nodes
+
+
+def part_one(puzzle_input):
+    sequence, nodes = parse_input(puzzle_input)
     return find_num_steps("AAA", nodes, sequence)
 
 
 def part_two(puzzle_input):
-    sequence = puzzle_input[0]
-    nodes = {match.group(1): (match.group(2), match.group(
-        3)) for input_string in puzzle_input[2:] if (match := PATTERN.match(input_string))}
-    start_nodes = [x for x in nodes.keys() if x.endswith("A")]
+    sequence, nodes = parse_input(puzzle_input)
+    start_nodes = [node for node in nodes.keys() if node.endswith("A")]
 
-    steps = []
-    for node in start_nodes:
-        steps.append(find_num_steps(node, nodes, sequence, True))
-
+    steps = [find_num_steps(node, nodes, sequence, True)
+             for node in start_nodes]
     return math.lcm(*steps)
 
 
