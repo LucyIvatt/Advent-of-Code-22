9570811fa1700728c7e4e6edb4c8576ab9a84b0d
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-09 19:24:00 +0000

    minor refactor of day 9

--- year_2023/day_09_mirage_maintenance/solution.py
+++ year_2023/day_09_mirage_maintenance/solution.py
@@ -3,28 +3,23 @@ from helpers.aoc_utils import input_data, time_function
 
 def solution(puzzle_input, backwards=False):
     extr_sum = 0
+
     for history in puzzle_input:
-        values = [int(num) for num in history.split()]
-        rows = [values]
+        rows = [[int(num) for num in history.split()]]
 
-        curr_row = values
+        while any(value != 0 for value in rows[-1]):
+            rows.append([rows[-1][i+1]-rows[-1][i]
+                        for i in range(len(rows[-1])-1)])
 
-        while not all(val == 0 for val in curr_row):
-            differences = [curr_row[i+1]-curr_row[i]
-                           for i in range(len(curr_row)-1)]
-            rows.append(differences)
-            curr_row = differences
-
-        for i in range(len(rows)-1, -1, -1):
-            if i == len(rows)-1:
+        for i in range(len(rows) - 1, -1, -1):
+            if i == len(rows) - 1:
                 rows[i].append(0)
-
-            elif not backwards:
-                rows[i].append(rows[i][-1] + rows[i+1][-1])
             else:
-                rows[i].insert(0, rows[i][0] - rows[i+1][0])
+                val = rows[i][0] - rows[i +
+                                        1][0] if backwards else rows[i][-1] + rows[i + 1][-1]
+                rows[i] = [val] + rows[i] if backwards else rows[i]+[val]
 
-        extr_sum += rows[0][-1] if not backwards else rows[0][0]
+        extr_sum += rows[0][0] if backwards else rows[0][-1]
 
     return extr_sum
 
