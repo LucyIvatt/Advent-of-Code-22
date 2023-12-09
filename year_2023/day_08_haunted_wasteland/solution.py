fef540bcb4d721e6070e5108c540f53e9b8b50cb
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-09 16:52:55 +0000

    added boilerplate for day 8, updated readme

---
+++ year_2023/day_08_haunted_wasteland/solution.py
@@ -0,0 +1,18 @@
+from helpers.aoc_utils import input_data, time_function
+
+puzzle_input = input_data("year_2023/day_08_haunted_wasteland/input.txt")
+
+def part_one(puzzle_input):
+    pass
+
+def part_two(puzzle_input):
+    pass
+
+p1, p1_time = time_function(part_one, puzzle_input)
+p2, p2_time = time_function(part_two, puzzle_input)
+
+print("--------------------------------------")
+print("Day 08: haunted_wasteland")
+print(f"Part One Answer: {p1} - [{p1_time:.4f} seconds]")
+print(f"Part Two Answer: {p2} - [{p2_time:.4f} seconds]")
+print("--------------------------------------")
