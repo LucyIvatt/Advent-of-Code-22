0d44f44f4f440d7b3cbe451cdc9d04b49b0fb83b
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-20 18:40:41 +0000

    finished day 14 p2

--- year_2023/day_14_parabolic_reflector_dish/solution.py
+++ year_2023/day_14_parabolic_reflector_dish/solution.py
@@ -1,13 +1,24 @@
 from helpers.aoc_utils import input_data, time_function, Direction
 
 
-def tilt_puzzle(puzzle_input):
+def save_state(grid):
+    return hash(tuple(map(tuple, grid)))
+
+
+def tilt_puzzle(puzzle_input, di):
+    r_start = 1 if di == Direction.NORTH else 0
+    r_end = (len(puzzle_input) -
+             1) if di == Direction.SOUTH else len(puzzle_input)
+    c_start = 1 if di == Direction.WEST else 0
+    c_end = len(puzzle_input[0]) - \
+        1 if di == Direction.EAST else len(puzzle_input[0])
+
     while True:
         isUpdating = False
-        for row in range(1, len(puzzle_input)):
-            for col in range(len(puzzle_input)):
+        for row in range(r_start, r_end):
+            for col in range(c_start, c_end):
                 if puzzle_input[row][col] == 'O':
-                    dr, dc = Direction.NORTH.value
+                    dr, dc = di.value
                     if puzzle_input[row + dr][col + dc] == ".":
                         puzzle_input[row + dr][col + dc] = "O"
                         puzzle_input[row][col] = "."
@@ -26,12 +37,37 @@ def calculate_load(tilted):
 
 def part_one(puzzle_input):
     puzzle_input = [list(string) for string in puzzle_input]
-    tilted = tilt_puzzle(puzzle_input)
+    tilted = tilt_puzzle(puzzle_input, Direction.NORTH)
     return calculate_load(tilted)
 
 
 def part_two(puzzle_input):
-    pass
+    puzzle_input = [list(string) for string in puzzle_input]
+    cycle = [Direction.NORTH, Direction.WEST, Direction.SOUTH, Direction.EAST]
+    cycle_num = 1_000_000_000
+    states = {}
+
+    c = 0
+    while c < cycle_num:
+        for direction in cycle:
+            puzzle_input = tilt_puzzle(puzzle_input, direction)
+
+        state = save_state(puzzle_input)
+
+        #fmt: off
+        if state in states and c < 500:
+            loop_length = c - states[state]
+            print(f"loop! c={c} is also {states[state]}, so loop length is {loop_length}")
+            print(f"you can fit {cycle_num // loop_length} full loops in, which puts you at {(cycle_num // loop_length) * loop_length}")
+            distance_to_goal = cycle_num - c
+            loop_length = c - states[state]
+            c = cycle_num - distance_to_goal % loop_length
+        #fmt: on
+
+        states[save_state(puzzle_input)] = c
+        c += 1
+
+    return calculate_load(puzzle_input)
 
 
 puzzle_input = input_data(
