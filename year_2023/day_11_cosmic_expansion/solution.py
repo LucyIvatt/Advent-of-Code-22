25bbe8c50c175bf65b79b833b2a2d0a582edf51b
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-20 11:46:08 +0000

    slight refactor of d11

--- year_2023/day_11_cosmic_expansion/solution.py
+++ year_2023/day_11_cosmic_expansion/solution.py
@@ -3,53 +3,35 @@ from itertools import combinations
 
 
 def get_expand_locations(puzzle_input):
-    rows, cols = [], []
-
-    # Checks and expands rows
-    for r in range(len(puzzle_input)):
-        if "#" not in puzzle_input[r]:
-            rows.append(r)
-
-    # Checks and expands columns
-    for c in range(len(puzzle_input[0])):
-        if not any(row[c] == "#" for row in puzzle_input):
-            cols.append(c)
+    rows = [r for r, row in enumerate(puzzle_input) if "#" not in row]
+    cols = [c for c in range(len(puzzle_input[0])) if all(
+        row[c] != "#" for row in puzzle_input)]
     return rows, cols
 
 
 def solution(puzzle_input, expansion_size):
     rows, cols = get_expand_locations(puzzle_input)
-
-    galaxy_locations = []
-    for i, row in enumerate(puzzle_input):
-        for j, value in enumerate(row):
-            if value == "#":
-                galaxy_locations.append((i, j))
+    galaxy_locations = [(i, j) for i, row in enumerate(puzzle_input)
+                        for j, value in enumerate(row) if value == "#"]
 
     pairs = list(combinations(galaxy_locations, 2))
-    sps = 0
+    shortest_paths = 0
 
     for pair in pairs:
-        pair_sp = 0
-        r1, c1 = pair[0]
-        r2, c2 = pair[1]
-
         r1, r2 = sorted([pair[0][0], pair[1][0]])
         c1, c2 = sorted([pair[0][1], pair[1][1]])
 
-        pair_sp += manhattan_distance(pair[0], pair[1])
+        shortest_paths += manhattan_distance(pair[0], pair[1])
 
         for r in rows:
-            if r in range(r1, r2+1):
-                pair_sp += expansion_size - 1
+            if r1 <= r <= r2:
+                shortest_paths += expansion_size - 1
 
         for c in cols:
-            if c in range(c1, c2+1):
-                pair_sp += expansion_size - 1
+            if c1 <= c <= c2:
+                shortest_paths += expansion_size - 1
 
-        sps += pair_sp
-
-    return sps
+    return shortest_paths
 
 
 puzzle_input = input_data("year_2023/day_11_cosmic_expansion/input.txt")
