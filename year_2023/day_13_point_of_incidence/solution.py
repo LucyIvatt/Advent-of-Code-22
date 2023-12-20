59fbaaa8fb794887d44c4b4121f03c92ba1ef83b
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-20 16:55:50 +0000

    finished d13 refactor

--- year_2023/day_13_point_of_incidence/solution.py
+++ year_2023/day_13_point_of_incidence/solution.py
@@ -1,26 +1,46 @@
 from helpers.aoc_utils import input_data, time_function, transpose_list
 from itertools import groupby
-import copy
+
+
+def split_patterns(puzzle_input):
+    """Splits the puzzle input into patterns by empty line."""
+    return [list(group) for key, group in groupby(puzzle_input, key=lambda x: x == "") if not key]
+
+
+def flip_symbol(pattern, row, col):
+    """Flips the symbol at the specified location from (. -> #) or (# -> .)"""
+
+    new = pattern[:]
+    new[row] = new[row][:col] + \
+        ('.' if new[row][col] == '#' else '#') + new[row][col+1:]
+    return new
 
 
 def scan_direction(pattern, horizontal=True, ignore=None):
-    pattern = pattern if horizontal else transpose_list(pattern)
+    """Checks for either horizontal lines or symmetry or vertical. If value is passed via 
+    ignore parameter, will not return a line of symmetry at this index.
+    Returns tuple in the form (bool, int | None) where the bool represents if a value was found, 
+    and int | None represents the found LoS location."""
 
-    for i in range(1, len(pattern)):
-        dx = i if i <= (len(pattern) / 2) else (len(pattern) - i)
-        if all(pattern[top] == pattern[bottom] for top, bottom in zip(range(i-dx, i), reversed(range(i, i+dx)))):
-            if ignore == None:
+    pattern = pattern if horizontal else transpose_list(pattern)
+    size = len(pattern)
+
+    for i in range(1, size):
+        dx = min(i, size - i)
+        top_range = range(i - dx, i)
+        bottom_range = reversed(range(i, i + dx))
+
+        if all(pattern[top] == pattern[bottom] for top, bottom in zip(top_range, bottom_range)):
+            if ignore is None or i != ignore:
                 return True, i
-            else:
-                if i != ignore:
-                    return True, i
+
     return False, None
 
 
 def part_one(puzzle_input):
-    patterns = [list(group) for key, group in groupby(
-        puzzle_input, key=lambda x: x == "") if not key]
+    """Finds line of symmetry in each pattern and calculates score accordingly."""
 
+    patterns = split_patterns(puzzle_input)
     total_sum = 0
 
     for pattern in patterns:
@@ -34,44 +54,35 @@ def part_one(puzzle_input):
 
 
 def part_two(puzzle_input):
-    patterns = [list(group) for key, group in groupby(
-        puzzle_input, key=lambda x: x == "") if not key]
+    """Finds line of symmetry in each pattern after flipping the correct symbol, 
+    ignoring the original line of symmetry, and calculates score accordingly."""
 
+    patterns = split_patterns(puzzle_input)
     total_sum = 0
-
     for pattern in patterns:
-        o_h_found, orig_h_loc = scan_direction(pattern, horizontal=True)
-        o_v_found, orig_v_loc = scan_direction(pattern, horizontal=False)
+        orig_h_loc = scan_direction(pattern, horizontal=True)[1]
+        orig_v_loc = scan_direction(pattern, horizontal=False)[1]
 
         for r in range(len(pattern)):
             for c in range(len(pattern[0])):
-                new_p = copy.deepcopy(pattern)
-
-                row = new_p[r]
-
-                if row[c] == "#":
-                    new_p[r] = row[:c] + "." + row[c+1:]
-                else:
-                    new_p[r] = row[:c] + "#" + row[c+1:]
+                new_p = flip_symbol(pattern, r, c)
 
                 h_found, h_loc = scan_direction(
                     new_p, horizontal=True, ignore=orig_h_loc)
                 v_found, v_loc = scan_direction(
                     new_p, horizontal=False, ignore=orig_v_loc)
 
-                if h_found and h_loc != orig_h_loc:
-                    total_sum += h_loc * 100
+                if (h_found or v_found):
+                    total_sum += h_loc * 100 if h_found else v_loc
                     break
-                elif v_found and v_loc != orig_v_loc:
-                    total_sum += v_loc
-                    break
-            if (h_found and h_loc != orig_h_loc) or (v_found and v_loc != orig_v_loc):
+
+            if (h_found or v_found):
                 break
 
     return total_sum
 
 
-puzzle_input = input_data("year_2023/day_13_point_of_incidence/input.txt")
+puzzle_input = input_data("year_2023/day_13_point_of_incidence/example.txt")
 
 p1, p1_time = time_function(part_one, puzzle_input)
 p2, p2_time = time_function(part_two, puzzle_input)
