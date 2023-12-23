9449e07d457a80d7826731b5a0852cc1a9259899
Lucy Ivatt <lucy.ivatt@xdesign.com>
2023-12-23 13:16:27 +0000

    began creating rule class for day 19

--- year_2023/day_19_aplenty/solution.py
+++ year_2023/day_19_aplenty/solution.py
@@ -1,10 +1,48 @@
 from helpers.aoc_utils import input_data, time_function
+import re
+
+PATTERN = re.compile(r'^([a-z])([<>]\d+):([a-z]+)$')
+
+
+class Rule():
+    def __init__(self, input_str) -> None:
+        match = PATTERN.match(input_str)
+        if match:
+            self.final = False
+            self.input = match.group(1)
+            self.operation = match.group(2)
+            self.destination = match.group(3)
+        else:
+            self.destination = input_str
+            self.final = True
+
+    def __repr__(self) -> str:
+        if not self.final:
+            return f"Rule({self.input=} {self.operation=} {self.destination=})"
+        else:
+            return f"Rule({self.destination=})"
+
+    def process_part(self, part) -> str:
+        if self.final:
+            return self.destination
+        else:
+            val = part[self.input]
+            if eval(f"{val}{self.operation}"):
+                return self.destination
+            else:
+                return None
+
 
 def part_one(puzzle_input):
-    pass
+    x = Rule("a<2006:qkq")
+    part = {"x": 0, "m": 1, "a": 2, "s": 3}
+    print(x)
+    print(x.process_part(part))
+
 
 def part_two(puzzle_input):
     pass
+
 
 def main():
     puzzle_input = input_data("year_2023/day_19_aplenty/input.txt")
@@ -18,5 +56,47 @@ def main():
     print(f"Part Two Answer: {p2} - [{p2_time:.4f} seconds]")
     print("--------------------------------------")
 
+
 if __name__ == "__main__":
     main()
+# a<2006:qkq,
+# m>2090:A,
+# rfg
+"""
+a<2006:qkq,
+m>2090:A,
+rfg
+
+object representing a rule: (maybe use eval?)
+    - letter than is compared
+    - operation
+    - number
+    - destination
+
+
+
+
+
+parts = list of dicts for the parts (x, m, a, s as keys)
+
+rules = dict of workflow with list of rules ({"in": rule1, rule2})
+
+list of Accepted
+
+for part in parts:
+    current_location = 'in'
+    while current_location is not R or A:
+        rule_id = 0
+        new_location = rules[current_location][rule_id].find_next_location
+        if new location and current location are the same, increment rule id:
+        otherwise reset rule id
+        current_location = new_location
+    
+    if current_location == "A":
+        append to list
+        
+
+
+
+
+"""
--- year_2023/day_19_aplenty/test.py
+++ year_2023/day_19_aplenty/test.py
@@ -2,6 +2,7 @@ import unittest
 
 from year_2023.day_19_aplenty.solution import input_data, part_one, part_two
 
+
 class TestDay19(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -12,8 +13,7 @@ class TestDay19(unittest.TestCase):
         """
         Tests Day 19 Part 1 using the example given in the scenario
         """
-        # self.assertEqual(part_one(self.__class__.example), 0)
-        pass
+        self.assertEqual(part_one(self.__class__.example), 19_114)
 
     def test_p1_actual(self):
         """
@@ -37,6 +37,7 @@ class TestDay19(unittest.TestCase):
         """
         # self.assertEqual(part_two(self.__class__.puzzle_input), 0)
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
