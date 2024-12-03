b888f71a3a45d42641e9bcea03ccc2c7cc507ce7
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-03 18:32:27 +0000

    Refactored day 3, updated README

--- typescript/year_2024/day_03_mull_it_over/solution.ts
+++ typescript/year_2024/day_03_mull_it_over/solution.ts
@@ -4,46 +4,45 @@ import { runPuzzle } from '../../utils/runPuzzle';
 
 type RegexMatch = { match: string; index: number; groups: number[] };
 
+// Matches "mul(x, y)" where x and y are 1-3 digit numbers
 const MUL_REGEX = /mul\((\d{1,3}),(\d{1,3})\)/g;
+
+// Matches "do()" or "don't()"
 const COND_REGEX = /do\(\)|don't\(\)/g;
 
-const findMatches = (input: string, re: RegExp): RegexMatch[] => {
-  return [...input.matchAll(re)].map((match) => ({
+const findMatches = (input: string, regex: RegExp): RegexMatch[] => {
+  return [...input.matchAll(regex)].map((match) => ({
     match: match[0],
-    index: match.index ?? 0,
+    index: match.index ?? -1,
     groups: match.slice(1).map(Number)
   }));
 };
 
 export const partOne = (puzzle_input: string[]) => {
   const instructions = puzzle_input.join('');
-
-  return findMatches(instructions, MUL_REGEX)
-    .reduce((acc, match) => (acc += match.groups[0] * match.groups[1]), 0)
-    .toString();
+  const result = findMatches(instructions, MUL_REGEX).reduce((acc, { groups: [a, b] }) => (acc += a * b), 0);
+  return result.toString();
 };
 
 export const partTwo = (puzzle_input: string[]) => {
   const instructions = puzzle_input.join('');
 
-  const mul_instructions = findMatches(instructions, MUL_REGEX);
-  const cond_instructions = findMatches(instructions, COND_REGEX);
+  const multiplications = findMatches(instructions, MUL_REGEX);
+  const conditions = findMatches(instructions, COND_REGEX);
 
-  return mul_instructions
-    .reduce((acc, instruction) => {
-      const earlierConditions = cond_instructions.filter((a) => a.index < instruction.index);
-      const conditional = earlierConditions[earlierConditions.length - 1];
+  const result = multiplications.reduce((sum, { groups: [a, b], index }) => {
+    const condition = conditions.filter((cond) => cond.index < index).at(-1);
 
-      if (!conditional || conditional.match === 'do()') {
-        return (acc += instruction.groups[0] * instruction.groups[1]);
-      }
+    if (!condition || condition.match === 'do()') {
+      sum += a * b;
+    }
+    return sum;
+  }, 0);
 
-      return acc;
-    }, 0)
-    .toString();
+  return result.toString();
 };
 
 if (require.main === module) {
-  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.INPUT));
+  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE_2));
   runPuzzle('03', 'mull_it_over', partOne, partTwo, puzzle_input);
 }
