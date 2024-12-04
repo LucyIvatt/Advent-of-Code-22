27eb4404496a324979eb9ec982b9ddd41f1dbac7
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-04 19:15:49 +0000

    refactored day 4

--- typescript/year_2024/day_04_ceres_search/solution.ts
+++ typescript/year_2024/day_04_ceres_search/solution.ts
@@ -3,46 +3,39 @@ import { InputFile, readPuzzleInput } from '../../utils/readFile';
 import { runPuzzle } from '../../utils/runPuzzle';
 import { Direction, Grid } from '../../utils/grids';
 
-export const partOne = (puzzle_input: string[]) => {
-  const grid = new Grid(puzzle_input.map((line) => line.split('')));
-  let xmasCount = 0;
+const countXmas = (i: number, j: number, grid: Grid<string>): number =>
+  Object.values(Direction).filter((direction) => {
+    const { values } = grid.walk(i, j, direction, 4);
+    return values.join('') === 'XMAS';
+  }).length;
 
-  for (let i = 0; i < grid.array.length; i++) {
-    for (let j = 0; j < grid.array[i].length; j++) {
-      const char = grid.array[i][j];
-      if (char === 'X') {
-        for (const direction of Object.values(Direction)) {
-          const { values } = grid.walk(i, j, direction, 4);
-          if (values.join('') === 'XMAS') xmasCount++;
-        }
-      }
-    }
-  }
-  return xmasCount.toString();
+const isMasOrSam = (values: string[]): boolean => ['MAS', 'SAM'].includes(values.join(''));
+
+export const partOne = (puzzleInput: string[]) => {
+  const grid = new Grid(puzzleInput.map((line) => line.split('')));
+
+  return grid.array
+    .flatMap((row, i) => row.map((cell, j) => (cell === 'X' ? countXmas(i, j, grid) : 0)))
+    .reduce((sum, count) => sum + count, 0)
+    .toString();
 };
 
-export const partTwo = (puzzle_input: string[]) => {
-  const grid = new Grid(puzzle_input.map((line) => line.split('')));
-  let x_masCount = 0;
+export const partTwo = (puzzleInput: string[]) => {
+  const grid = new Grid(puzzleInput.map((line) => line.split('')));
 
-  for (let i = 0; i < grid.array.length; i++) {
-    for (let j = 0; j < grid.array[i].length; j++) {
-      const { values: se_search } = grid.walk(i, j, Direction.SouthEast, 3);
-      const { values: sw_search } = grid.walk(i, j + 2, Direction.SouthWest, 3);
-
-      if (
-        (se_search.join('') === 'MAS' || se_search.join('') === 'SAM') &&
-        (sw_search.join('') === 'MAS' || sw_search.join('') === 'SAM')
-      ) {
-        x_masCount++;
-      }
-    }
-  }
-
-  return x_masCount.toString();
+  return grid.array
+    .flatMap((row, i) =>
+      row.map((_, j) => {
+        const { values: se_sequence } = grid.walk(i, j, Direction.SouthEast, 3);
+        const { values: sw_sequence } = grid.walk(i, j + 2, Direction.SouthWest, 3);
+        return isMasOrSam(se_sequence) && isMasOrSam(sw_sequence) ? 1 : (0 as number);
+      })
+    )
+    .reduce((sum, count) => sum + count, 0)
+    .toString();
 };
 
 if (require.main === module) {
-  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.INPUT));
+  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE));
   runPuzzle('04', 'ceres_search', partOne, partTwo, puzzle_input);
 }
