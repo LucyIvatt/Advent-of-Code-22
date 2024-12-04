84154175eb9d86cdcbab5e6d24e1a3b1d7ced770
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-04 21:52:13 +0000

    Updated day 4 to only check A cells for p2

--- typescript/year_2024/day_04_ceres_search/solution.ts
+++ typescript/year_2024/day_04_ceres_search/solution.ts
@@ -25,10 +25,13 @@ export const partTwo = (puzzleInput: string[]) => {
 
   return grid.array
     .flatMap((row, i) =>
-      row.map((_, j) => {
-        const { values: se_sequence } = grid.walk(i, j, Direction.SouthEast, 3);
-        const { values: sw_sequence } = grid.walk(i, j + 2, Direction.SouthWest, 3);
-        return isMasOrSam(se_sequence) && isMasOrSam(sw_sequence) ? 1 : (0 as number);
+      row.map((cell, j) => {
+        if (cell === 'A' && i > 0 && i < grid.array.length - 1 && j > 0 && j < grid.array[0].length - 1) {
+          const { values: se_sequence } = grid.walk(i - 1, j - 1, Direction.SouthEast, 3);
+          const { values: sw_sequence } = grid.walk(i - 1, j + 1, Direction.SouthWest, 3);
+          return isMasOrSam(se_sequence) && isMasOrSam(sw_sequence) ? 1 : (0 as number);
+        }
+        return 0;
       })
     )
     .reduce((sum, count) => sum + count, 0)
@@ -36,6 +39,6 @@ export const partTwo = (puzzleInput: string[]) => {
 };
 
 if (require.main === module) {
-  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE));
+  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.INPUT));
   runPuzzle('04', 'ceres_search', partOne, partTwo, puzzle_input);
 }
