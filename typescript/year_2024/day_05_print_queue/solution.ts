92a0446a492ef29a5fe8547a276645dde7d48546
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-05 11:05:47 +0000

    refactored day 5

--- typescript/year_2024/day_05_print_queue/solution.ts
+++ typescript/year_2024/day_05_print_queue/solution.ts
@@ -3,56 +3,51 @@ import { InputFile, readPuzzleInput } from '../../utils/readFile';
 import { runPuzzle } from '../../utils/runPuzzle';
 import { count } from '../../utils/misc';
 
-export const partOne = (puzzle_input: string[]) => {
+const formatInputs = (puzzle_input: string[]) => {
   const delimiterIndex = puzzle_input.indexOf('');
 
-  const orderingRules = puzzle_input.slice(0, delimiterIndex).map((rule) => rule.split('|').map(Number));
+  const rules = puzzle_input.slice(0, delimiterIndex).map((rule) => rule.split('|').map(Number));
   const lists = puzzle_input.slice(delimiterIndex + 1).map((list) => list.split(',').map(Number));
 
-  let sum = 0;
+  return { rules, lists };
+};
+
+const getValidRules = (list: number[], rules: number[][]): number[][] => {
+  const set = new Set(list);
+  return rules.filter(([a, b]) => set.has(a) && set.has(b));
+};
+
+const processLists = (lists: number[][], rules: number[][], fixBrokenOrders = false): number => {
+  let totalSum = 0;
 
   for (const list of lists) {
-    const validRules = orderingRules.filter(([a, b]) => list.includes(a) && list.includes(b));
+    const validRules = getValidRules(list, rules);
     const counts = count(validRules.map(([, b]) => b));
 
-    if (
-      list.every((item, i) => {
-        return counts[item] === i || (!counts[item] && i === 0);
-      })
-    ) {
-      sum += list[Math.floor(list.length / 2)];
+    const isValid = list.every((item, i) => counts[item] === i || (!counts[item] && i === 0));
+
+    if (!fixBrokenOrders && isValid) totalSum += list[Math.floor(list.length / 2)];
+
+    if (fixBrokenOrders && !isValid) {
+      list.sort((a, b) => (counts[a] || 0) - (counts[b] || 0));
+      totalSum += list[Math.floor(list.length / 2)];
     }
   }
 
-  return sum.toString();
+  return totalSum;
+};
+
+export const partOne = (puzzle_input: string[]) => {
+  const { rules, lists } = formatInputs(puzzle_input);
+  return processLists(lists, rules).toString();
 };
 
 export const partTwo = (puzzle_input: string[]) => {
-  const delimiterIndex = puzzle_input.indexOf('');
-
-  const orderingRules = puzzle_input.slice(0, delimiterIndex).map((rule) => rule.split('|').map(Number));
-  const lists = puzzle_input.slice(delimiterIndex + 1).map((list) => list.split(',').map(Number));
-
-  let sum = 0;
-
-  for (const list of lists) {
-    const validRules = orderingRules.filter(([a, b]) => list.includes(a) && list.includes(b));
-    const counts = count(validRules.map(([, b]) => b));
-
-    if (
-      !list.every((item, i) => {
-        return counts[item] === i || (!counts[item] && i === 0);
-      })
-    ) {
-      list.sort((a, b) => (counts[a] || 0) - (counts[b] || 0));
-      sum += list[Math.floor(list.length / 2)];
-    }
-  }
-
-  return sum.toString();
+  const { rules, lists } = formatInputs(puzzle_input);
+  return processLists(lists, rules, true).toString();
 };
 
 if (require.main === module) {
-  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.INPUT));
+  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE));
   runPuzzle('05', 'print_queue', partOne, partTwo, puzzle_input);
 }
