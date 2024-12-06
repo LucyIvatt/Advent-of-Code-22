4e5ce792bb714f6f3c58a3ed6bb0e51e8baf5b74
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-06 18:44:36 +0000

    completed day 6 p2 before refactor

--- typescript/year_2024/day_06_guard_gallivant/solution.ts
+++ typescript/year_2024/day_06_guard_gallivant/solution.ts
@@ -2,90 +2,101 @@ import path from 'path';
 import { Direction, Grid, rotate90Degrees } from '../../utils/grid';
 import { InputFile, readPuzzleInput } from '../../utils/readFile';
 import { runPuzzle } from '../../utils/runPuzzle';
-import { delay } from '../../utils/time';
 
-export const partOne = async (puzzle_input: string[]) => {
-  const grid = new Grid(puzzle_input.map((a) => a.split('')));
-  const visited = new Set();
+export const partOne = async (puzzleInput: string[]) => {
+  const grid = new Grid(puzzleInput.map((row) => row.split('')));
+  const visited = new Set<string>();
 
-  let loc: [number, number] = grid.find('^')[0];
-  let dir = Direction.North;
+  let [x, y] = grid.find('^')[0];
+  grid.array[x][y] = '.';
+
+  let direction = Direction.North;
+
+  visited.add(`${x},${y}`);
+
   let escaped = false;
 
-  visited.add(`${loc[0]},${loc[1]}`);
-
   while (!escaped) {
-    let nextVal, nextPos;
+    let nextPos: [number, number] | undefined;
+    let nextVal: string | undefined;
+    let foundValidMove = false;
 
-    while (!nextVal || !nextPos || (nextVal !== '.' && nextVal !== 'X')) {
-      if (nextVal && nextPos && nextVal !== '.' && nextVal !== 'X') dir = rotate90Degrees(dir);
+    for (let i = 0; i < 4; i++) {
       try {
-        ({ value: nextVal, position: nextPos } = grid.getAdjacent(loc[0], loc[1], dir));
+        ({ value: nextVal, position: nextPos } = grid.getAdjacent(x, y, direction));
+
+        if (nextVal === '.') {
+          foundValidMove = true;
+          break;
+        }
+
+        direction = rotate90Degrees(direction);
       } catch {
         escaped = true;
         break;
       }
     }
 
-    grid.array[loc[0]][loc[1]] = 'X';
-    // console.clear();
-    // console.log(grid.toString(false));
-    // await delay(75);
-
     if (escaped) break;
 
-    if (nextPos) {
-      loc = nextPos;
-      visited.add(`${loc[0]},${loc[1]}`);
+    if (foundValidMove && nextPos) {
+      [x, y] = nextPos;
+      visited.add(`${x},${y}`);
     }
   }
 
   return visited.size.toString();
 };
 
-export const partTwo = (puzzle_input: string[]) => {
+export const partTwo = (puzzleInput: string[]) => {
   let loops = 0;
 
-  for (let i = 0; i < puzzle_input.length; i++) {
-    for (let j = 0; j < puzzle_input[i].length; j++) {
-      const grid = new Grid(puzzle_input.map((a) => [...a.split('')]));
-      let loc: [number, number] = grid.find('^')[0];
-      let dir = Direction.North;
-      let escaped = false;
-      const visited = new Set();
+  for (let i = 0; i < puzzleInput.length; i++) {
+    for (let j = 0; j < puzzleInput[i].length; j++) {
+      const grid = new Grid(puzzleInput.map((row) => row.split('')));
 
-      visited.add(`${loc[0]},${loc[1]},${dir}`);
+      const visited = new Set<string>();
 
-      if (i === loc[0] && j === loc[1]) break;
+      let [x, y] = grid.find('^')[0];
+      let direction = Direction.North;
 
+      grid.array[x][y] = '.';
       grid.array[i][j] = '#';
 
-      while (!escaped) {
-        let nextVal, nextPos;
+      visited.add(`${x},${y},${direction}`);
 
-        while (!nextVal || !nextPos || (nextVal !== '.' && nextVal !== 'X')) {
-          if (nextVal && nextPos && nextVal !== '.' && nextVal !== 'X') dir = rotate90Degrees(dir);
+      let escaped = false;
+
+      while (!escaped) {
+        let nextPos: [number, number] | undefined;
+        let nextVal: string | undefined;
+        let foundValidMove = false;
+
+        for (let i = 0; i < 4; i++) {
           try {
-            ({ value: nextVal, position: nextPos } = grid.getAdjacent(loc[0], loc[1], dir));
+            ({ value: nextVal, position: nextPos } = grid.getAdjacent(x, y, direction));
+
+            if (nextVal === '.') {
+              foundValidMove = true;
+              break;
+            }
+
+            direction = rotate90Degrees(direction);
           } catch {
             escaped = true;
             break;
           }
         }
 
-        grid.array[loc[0]][loc[1]] = 'X';
+        if (escaped) break;
 
-        if (escaped) {
-          break;
-        }
-
-        if (nextPos) {
-          loc = nextPos;
-          if (visited.has(`${loc[0]},${loc[1]},${dir}`)) {
+        if (foundValidMove && nextPos) {
+          [x, y] = nextPos;
+          if (visited.has(`${x},${y},${direction}`)) {
             loops++;
             break;
           }
-          visited.add(`${loc[0]},${loc[1]},${dir}`);
+          visited.add(`${x},${y},${direction}`);
         }
       }
     }
@@ -94,6 +105,6 @@ export const partTwo = (puzzle_input: string[]) => {
 };
 
 if (require.main === module) {
-  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE));
+  const puzzle_input = readPuzzleInput(path.resolve(__dirname, InputFile.INPUT));
   runPuzzle('06', 'guard_gallivant', partOne, partTwo, puzzle_input);
 }
