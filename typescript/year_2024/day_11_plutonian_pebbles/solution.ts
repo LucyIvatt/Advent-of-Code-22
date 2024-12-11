b6da25c93195fb09a2b3f7a4f6c40e51e2579e92
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-11 20:32:10 +0000

    completed day 11 before refactor

---
+++ typescript/year_2024/day_11_plutonian_pebbles/solution.ts
@@ -0,0 +1,55 @@
+import path from 'path';
+import { InputFile, readPuzzleInput } from '../../utils/readFile';
+import { runPuzzle } from '../../utils/runPuzzle';
+
+const updateMap = (stone: number, num: number, map: Map<number, number>) => {
+  map.set(stone, (map.get(stone) ?? 0) + num);
+};
+
+const shiftStone = (stone: number): number[] => {
+  if (stone == 0) return [1];
+
+  const stoneString = stone.toString();
+  const length = stoneString.length;
+
+  return length % 2 === 0
+    ? [Number(stoneString.substring(0, length / 2)), Number(stoneString.substring(length / 2))]
+    : [stone * 2024];
+};
+
+const processBlinks = (stones: number[], blinks: number) => {
+  let stoneCounts = new Map<number, number>();
+  stones.forEach((s) => stoneCounts.set(s, 1));
+
+  for (let i = 0; i < blinks; i++) {
+    const updatedStones = new Map<number, number>();
+    for (const [s, num] of stoneCounts) {
+      stoneCounts.delete(s);
+
+      const newStones = shiftStone(s);
+
+      for (const x of newStones) updateMap(x, num, updatedStones);
+    }
+    stoneCounts = updatedStones;
+  }
+
+  let ans = 0;
+  for (const count of stoneCounts.values()) ans += count;
+
+  return ans.toString();
+};
+
+export const partOne = (puzzleInput: string[]) => {
+  const stones = puzzleInput[0].split(' ').map(Number);
+  return processBlinks(stones, 25);
+};
+
+export const partTwo = (puzzleInput: string[]) => {
+  const stones = puzzleInput[0].split(' ').map(Number);
+  return processBlinks(stones, 75);
+};
+
+if (require.main === module) {
+  const puzzleInput = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE));
+  runPuzzle('11', 'plutonian_pebbles', partOne, partTwo, puzzleInput);
+}
