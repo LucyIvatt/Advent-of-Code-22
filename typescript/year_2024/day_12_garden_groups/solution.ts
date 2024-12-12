5dfbf037093a1fc42f25d66df32463e47921f3f5
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-12 22:39:23 +0000

    started refactoring day 12

--- typescript/year_2024/day_12_garden_groups/solution.ts
+++ typescript/year_2024/day_12_garden_groups/solution.ts
@@ -1,20 +1,29 @@
 import path from 'path';
 import { InputFile, readPuzzleInput } from '../../utils/readFile';
 import { runPuzzle } from '../../utils/runPuzzle';
-import { Coord, DIAGONAL_DIRECTIONS, Direction, Grid, rotate } from '../../utils/grid';
+import { Coord, DIAGONAL_DIRECTIONS, Direction, Grid, rotate, STRAIGHT_DIRECTIONS } from '../../utils/grid';
 
+// Rotate directions by 45 degrees and find the common direction
+export const getDiagonalDirectionForLShape = (directions: Direction[]): Direction => {
+  const [dir1, dir2] = directions;
+
+  const diagonals1 = [rotate(dir1, 45), rotate(dir1, -45)];
+  const diagonals2 = [rotate(dir2, 45), rotate(dir2, -45)];
+
+  const [diagonal] = diagonals1.filter((d) => diagonals2.includes(d));
+  return diagonal;
+};
+
+// finds middle direction by getting the direction 90 degrees left and right and checking if the array includes them
 export const getDiagonalDirectionsForTShape = (directions: Direction[]): Direction[] => {
-  const [d1, d2, d3] = directions;
-  const stem = [d1, d2, d3].find(
+  const middle = directions.find(
     (dir) => directions.includes(rotate(dir, 90)) && directions.includes(rotate(dir, -90))
   )!;
 
-  const diagonal1 = rotate(stem, 45); // Clockwise diagonal
-  const diagonal2 = rotate(stem, -45); // Counterclockwise diagonal
+  const diagonal1 = rotate(middle, 45); // Clockwise diagonal
+  const diagonal2 = rotate(middle, -45); // Counterclockwise diagonal
   return [diagonal1, diagonal2];
 };
-
-const VALID_DIRECTIONS = [Direction.North, Direction.East, Direction.South, Direction.West];
 
 const coordEquals = (a: Coord, b: Coord): boolean => a[0] === b[0] && a[1] === b[1];
 const coordInList = (coord: Coord, list: Coord[]): boolean => list.some((c) => coordEquals(c, coord));
@@ -44,19 +53,19 @@ export class Region {
   getSecondPrice = () => this.getArea() * this.corners;
 }
 
-const getOpenSides = (i: number, j: number, grid: Grid<string>, region: Region) => {
+const findOpenSides = (i: number, j: number, grid: Grid<string>, region: Region) => {
   const openSides = [];
   const typeOfPlant = grid.array[i][j];
-  const directionOfMatch = [];
+  const openDirections = [];
   let perimeter = 0;
 
-  for (const dir of VALID_DIRECTIONS) {
+  for (const direction of STRAIGHT_DIRECTIONS) {
     try {
-      const { value, position } = grid.getAdjacent(i, j, dir);
+      const { value, position } = grid.getAdjacent(i, j, direction);
       if (typeOfPlant !== value) perimeter += 1;
       else {
         if (!coordInList(position, region.locations)) openSides.push(position);
-        directionOfMatch.push(dir);
+        openDirections.push(direction);
       }
     } catch {
       perimeter += 1;
@@ -64,22 +73,25 @@ const getOpenSides = (i: number, j: number, grid: Grid<string>, region: Region) 
   }
 
   let corners = 0;
-  if (directionOfMatch.length === 1) corners = 2; // end of tunnel, must have 2 corners
+
+  if (openDirections.length === 0) corners = 4; // single cell region, must have 4 corners
+  if (openDirections.length === 1) corners = 2; // end of a tunnel, must always have 2 corners
 
   // no corners if opposite ends open (but if L shape could have 1 or 2 corners)
-  if (directionOfMatch.length === 2 && directionOfMatch[0] !== rotate(directionOfMatch[1], 180)) {
-    const diag = getDiagonalDirection(directionOfMatch[0], directionOfMatch[1])!;
+  if (openDirections.length === 2 && openDirections[0] !== rotate(openDirections[1], 180)) {
+    const diag = getDiagonalDirectionForLShape(openDirections);
     try {
       const diagonal = grid.getAdjacent(i, j, diag);
-      if (diagonal.value === typeOfPlant) corners = 1;
-      else corners = 2;
+      if (diagonal.value === typeOfPlant)
+        corners = 1; // valid position directly diagonal from L shape corneer (no addtional corner needed)
+      else corners = 2; // corner direction diagonal from L shape corner
     } catch {
-      corners = 2;
+      corners = 2; // corner direction diagonal from L shape corner
     }
   }
-  // could have no corners, 1, or even 2 if T shaped
-  if (directionOfMatch.length === 3) {
-    const diagonalDirections = getDiagonalDirectionsForTShape(directionOfMatch);
+  // T shaped opening - could have 2 additional corners
+  if (openDirections.length === 3) {
+    const diagonalDirections = getDiagonalDirectionsForTShape(openDirections);
 
     let invalidDiags = 0;
     for (const diagDirec of diagonalDirections) {
@@ -92,9 +104,9 @@ const getOpenSides = (i: number, j: number, grid: Grid<string>, region: Region) 
     }
     corners = invalidDiags;
   }
-  if (directionOfMatch.length === 0) corners = 4; // single cell region, must have 4 corners
 
-  if (directionOfMatch.length === 4) {
+  // completely open space, could have up to 4 additional corners
+  if (openDirections.length === 4) {
     let invalidDiags = 0;
     for (const diagDirec of DIAGONAL_DIRECTIONS) {
       try {
@@ -109,23 +121,6 @@ const getOpenSides = (i: number, j: number, grid: Grid<string>, region: Region) 
 
   return { openSides, perimeter, corners };
 };
-
-function getDiagonalDirection(dir1: Direction, dir2: Direction): Direction | null {
-  const directionSet = new Set([dir1, dir2]);
-
-  if (directionSet.has(Direction.North) && directionSet.has(Direction.East)) {
-    return Direction.NorthEast;
-  } else if (directionSet.has(Direction.East) && directionSet.has(Direction.South)) {
-    return Direction.SouthEast;
-  } else if (directionSet.has(Direction.South) && directionSet.has(Direction.West)) {
-    return Direction.SouthWest;
-  } else if (directionSet.has(Direction.West) && directionSet.has(Direction.North)) {
-    return Direction.NorthWest;
-  }
-
-  return null; // Return null if the directions are not at a 90-degree angle
-}
-
 const findRegions = (plants: Grid<string>) => {
   let processedCoords: Coord[] = [];
   const regions: Region[] = [];
@@ -142,7 +137,7 @@ const findRegions = (plants: Grid<string>) => {
         while (coordsToProcess.length > 0) {
           const coordToProcess = coordsToProcess[0];
 
-          const { openSides, perimeter, corners } = getOpenSides(coordToProcess[0], coordToProcess[1], plants, region);
+          const { openSides, perimeter, corners } = findOpenSides(coordToProcess[0], coordToProcess[1], plants, region);
           region.perimeter += perimeter;
           region.corners += corners;
 
