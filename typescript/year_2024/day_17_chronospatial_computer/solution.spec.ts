79e7dc31f5f56eda509831635be2d8acdbee88ec
Lucy Ivatt <lucyivatt@gmail.com>
2024-12-17 15:48:18 +0000

    updated unit tests day 17 and refactored

--- typescript/year_2024/day_17_chronospatial_computer/solution.spec.ts
+++ typescript/year_2024/day_17_chronospatial_computer/solution.spec.ts
@@ -1,34 +1,32 @@
-import path from "path";
-import { InputFile, readPuzzleInput } from "../../utils/readFile";
-import { partOne, partTwo } from "./solution";
+import path from 'path';
+import { InputFile, readPuzzleInput } from '../../utils/readFile';
+import { partOne, partTwo } from './solution';
 
-describe("TestDay17", () => {
-  const exampleInput = readPuzzleInput(
-    path.resolve(__dirname, InputFile.EXAMPLE),
-  );
+describe('TestDay17', () => {
+  const exampleInput = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE));
   const input = readPuzzleInput(path.resolve(__dirname, InputFile.INPUT));
 
-  describe("Part one", () => {
-    it("should return correct answer for the example input", () => {
+  describe('Part one', () => {
+    it('should return correct answer for the example input', () => {
       const result = partOne(exampleInput);
-      expect(result).toBe("Part 1 Answer");
+      expect(result).toBe('4,6,3,5,6,3,5,2,1,0');
     });
 
-    it("should return correct answer for the real input", () => {
+    it('should return correct answer for the real input', () => {
       const result = partOne(input);
-      expect(result).toBe("Part 1 Answer");
+      expect(result).toBe('6,2,7,2,3,1,6,0,5');
     });
   });
 
-  describe("Part two", () => {
-    it("should return correct answer for the example input", () => {
+  describe('Part two', () => {
+    it('should return correct answer for the example input', () => {
       const result = partTwo(exampleInput);
-      expect(result).toBe("Part 2 Answer");
+      expect(result).toBe('Part 2 Answer');
     });
 
-    it("should return correct answer for the real input", () => {
+    it('should return correct answer for the real input', () => {
       const result = partTwo(input);
-      expect(result).toBe("Part 2 Answer");
+      expect(result).toBe('Part 2 Answer');
     });
   });
 });
--- typescript/year_2024/day_17_chronospatial_computer/solution.ts
+++ typescript/year_2024/day_17_chronospatial_computer/solution.ts
@@ -1,13 +1,12 @@
-import path, { join } from 'path';
+import path from 'path';
 import { InputFile, readPuzzleInput } from '../../utils/readFile';
 import { runPuzzle } from '../../utils/runPuzzle';
 import { extractNumbers } from '../day_13_claw_contraption/solution';
-import { SQRT1_2 } from 'mathjs';
 
 const formatInput = (puzzleInput: string[]) => {
-  const [A, B, C] = puzzleInput.filter((line, i) => i < 3).map((line) => extractNumbers(line)[0]);
+  const A = extractNumbers(puzzleInput[0])[0];
   const program = extractNumbers(puzzleInput[4]);
-  return { A, B, C, program };
+  return { A, program };
 };
 
 const getComboOperand = (operand: number, A: number, B: number, C: number) => {
@@ -18,86 +17,64 @@ const getComboOperand = (operand: number, A: number, B: number, C: number) => {
   return NaN;
 };
 
-export const partOne = (puzzleInput: string[]) => {
-  const program = [2, 4, 1, 3, 7, 5, 1, 5, 0, 3, 4, 3, 5, 5, 3, 0];
+const runProgram = (instructions: number[], A: number): string => {
   const output = [];
-
-  let A = 47006051;
-  let B = 0;
-  let C = 0;
+  let registerB = 0;
+  let registerC = 0;
   let pointer = 0;
 
-  while (pointer < program.length - 1) {
-    const opcode = program[pointer];
-    const operand = program[pointer + 1];
+  while (pointer < instructions.length - 1) {
+    const opcode = instructions[pointer];
+    const operand = instructions[pointer + 1];
+    const comboOperand = getComboOperand(operand, A, registerB, registerC);
 
     switch (opcode) {
-      case 0: {
-        // adv instruction
-        A = Math.floor(A / Math.pow(2, getComboOperand(operand, A, B, C)));
+      case 0:
+        A = Math.floor(A / Math.pow(2, comboOperand));
         pointer += 2;
         break;
-      }
-      case 1: {
-        // bxl instruction
-        B = B ^ operand;
+      case 1:
+        registerB ^= operand;
         pointer += 2;
         break;
-      }
-      case 2: {
-        // bst instruction
-        const combo = getComboOperand(operand, A, B, C);
-        B = combo % 8;
+      case 2:
+        registerB = comboOperand % 8;
         pointer += 2;
         break;
-      }
-      case 3: {
-        // jnz instruction
-        if (A === 0) pointer += 2;
-        else pointer = operand;
+      case 3:
+        pointer = A === 0 ? pointer + 2 : operand;
         break;
-      }
-      case 4: {
-        // bxc instruction
-        B = B ^ C;
+      case 4:
+        registerB ^= registerC;
         pointer += 2;
         break;
-      }
-      case 5: {
-        // out instruction
-        const combo = getComboOperand(operand, A, B, C);
-        output.push(combo % 8);
+      case 5:
+        output.push(comboOperand % 8);
         pointer += 2;
         break;
-      }
-      case 6: {
-        // bdv instruction
-        B = Math.floor(A / Math.pow(2, getComboOperand(operand, A, B, C)));
+      case 6:
+        registerB = Math.floor(A / Math.pow(2, comboOperand));
         pointer += 2;
         break;
-      }
-      case 7: {
-        // cdv instruction
-        C = Math.floor(A / Math.pow(2, getComboOperand(operand, A, B, C)));
+      case 7:
+        registerC = Math.floor(A / Math.pow(2, comboOperand));
         pointer += 2;
         break;
-      }
     }
   }
-
-  console.log('A', A);
-  console.log('B', B);
-  console.log('C', C);
-  console.log(output.join(','));
-
-  return 'Part 1 Answer';
+  return output.join(',');
 };
 
+export const partOne = (puzzleInput: string[]) => {
+  const { A, program } = formatInput(puzzleInput);
+  console.log(program);
+  return runProgram(program, A);
+};
 export const partTwo = (puzzleInput: string[]) => {
   return 'Part 2 Answer';
 };
 
 if (require.main === module) {
-  const puzzleInput = readPuzzleInput(path.resolve(__dirname, InputFile.EXAMPLE));
+  const puzzleInput = readPuzzleInput(path.resolve(__dirname, InputFile.INPUT));
   runPuzzle('17', 'chronospatial_computer', partOne, partTwo, puzzleInput);
 }
